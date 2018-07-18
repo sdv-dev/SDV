35e8c00b8672169586340e595ff9277a5647fe65
amontane <andrew@dhcp-18-111-46-36.dyn.MIT.EDU>
2018-07-18 16:11:10 -0400

    adding a class for sdv

--- sdv/sdv.py
+++ sdv/sdv.py
@@ -1,30 +1,53 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
-import numpy as np
-from DataNavigator import DataNavigator
-from Sampler import Sampler
-from Modeler import Modeler
-from utils import load_model
+from sdv.DataNavigator import *
+from sdv.Sampler import Sampler
+from sdv.Modeler import Modeler
+import pickle
 
-np.set_printoptions(threshold=np.inf)
+DATA_LOADERS = {
+    'csv': CSVDataLoader
+}
 
-# Try to incorporate rdt
-dn = DataNavigator('../demo/Airbnb_demo_meta.json')
-dn.transform_data()
-print('Data', dn.data)
-print('child map', dn.child_map)
-print('parent map', dn.parent_map)
-print('transformed data', dn.transformed_data)
 
-modeler = Modeler(dn)
-modeler.model_database()
-modeler.save_model('example2')
-# modeler = load_model('models/example3.pkl')
-print('customers table', modeler.models['users'].data)
-print('orders table', modeler.models['sessions'].data)
-sampler = Sampler(dn, modeler)
-print('generated users', sampler.sample_rows('users', 2))
-print('generated sessions', sampler.sample_rows('sessions', 10))
-print('sampling everything', sampler.sample_all())
-# print('sampling specific table', sampler.sample_table('users'))
+class SDV:
+    """ class to do modeling and sampling all in one """
+    def __init__(self, meta_file_name, data_loader_type='csv'):
+        """ initializes sdv class """
+        data_loader = DATA_LOADERS[data_loader_type](meta_file_name)
+        self.dn = data_loader.load_data()
+        # transform data
+        self.dn.transform_data()
+        self.modeler = Modeler(self.dn)
+        self.sampler = None
+
+    def model_database(self):
+        """ has this sdv's modeler model the database """
+        self.modeler.model_database()
+
+    def sample_rows(self, table_name, num_rows):
+        """ Calls Sampler's sample rows """
+        if self.sampler is None:
+            self.sampler = Sampler(self.dn, self.modeler)
+        return self.sampler.sample_rows(table_name, num_rows)
+
+    def sample_table(self, table_name):
+        """ Calls Sampler's sample table """
+        if self.sampler is None:
+            self.sampler = Sampler(self.dn, self.modeler)
+        return self.sampler.sample_table(table_name)
+
+    def sample_all(self, num_rows=5):
+        """ Calls Sampler's sample all """
+        if self.sampler is None:
+            self.sampler = Sampler(self.dn, self.modeler)
+        return self.sampler.sample_all(num_rows)
+
+    def load_modeler(self, filename):
+        """ Loads model from filename
+        Args:
+            filename (string): path of file to load
+        """
+        with open(filename, 'rb') as input:
+            self.modeler = pickle.load(input)
