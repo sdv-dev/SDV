92748bc080a5aad6d4c07a896822e354dc18e5a9
amontane <andrew@MacBook-Pro-2.local>
2018-08-27 21:13:55 -0400

    sampler and modeler code review improvements

--- demo_data_downloader.py
+++ demo_data_downloader.py
@@ -6,9 +6,6 @@ from botocore.client import Config
 
 
 BUCKET_NAME = 'hdi-demos'  # replace with your bucket name
-# sessions_KEY = 'sdv-demo/sessions_demo.csv'
-# users_KEY = 'sdv-demo/users_demo.csv'
-# meta_KEY = 'sdv-demo/Airbnb_demo_meta.json'
 bio_KEY = 'sdv-demo/biodegradability.zip'
 airbnb_demo_KEY = 'sdv-demo/airbnb_demo.zip'
 airbnb_complete_KEY = 'sdv-demo/airbnb_complete.zip'
@@ -22,11 +19,6 @@ if not os.path.exists('demo'):
     os.makedirs('demo')
     # try to download files from s3
     try:
-        # s3.Bucket(BUCKET_NAME).download_file(sessions_KEY,
-        #                                      'demo/sessions_demo.csv')
-        # s3.Bucket(BUCKET_NAME).download_file(users_KEY, 'demo/users_demo.csv')
-        # s3.Bucket(BUCKET_NAME).download_file(meta_KEY,
-        #                                      'demo/Airbnb_demo_meta.json')
         s3.Bucket(BUCKET_NAME).download_file(bio_KEY,
                                              'demo/biodegradability.zip')
         s3.Bucket(BUCKET_NAME).download_file(airbnb_demo_KEY,
