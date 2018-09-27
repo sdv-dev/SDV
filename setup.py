b3d1640636b2d67a8e2a29f67cd8918ba290a90a
Manuel Alvarez <manuel@pythiac.com>
2018-09-27 20:30:45 +0200

    Bump version: 0.1.0 → 0.1.1-dev

--- setup.py
+++ setup.py
@@ -77,6 +77,6 @@ setup(
     test_suite='tests',
     tests_require=test_require,
     url='https://github.com/HDI-Project/sdv',
-    version='0.1.0',
+    version='0.1.1-dev',
     zip_safe=False,
 )
