5c7f0ef0b0d8167cdd398a26be2cac9f256e854a
Manuel Alvarez <manuel@pythiac.com>
2019-01-22 17:32:38 +0100

    Raise exception on unsupported dataset structures

--- sdv/sdv.py
+++ sdv/sdv.py
@@ -11,41 +11,72 @@ from sdv.sampler import Sampler
 
 
 class SDV:
-    """Class to do modeling and sampling all in one."""
+    """Class to do modeling and sampling all in one.
+
+    Args:
+        meta_file_name (str): Path to the metadata file.
+        data_loader_type (str)
+    """
 
     def __init__(self, meta_file_name, data_loader_type='csv'):
-        """Initialize sdv class."""
         self.meta_file_name = meta_file_name
         self.sampler = None
 
+    def _check_unsupported_dataset_structure(self):
+        """Checks that no table has two parents."""
+        tables = self.dn.tables.keys()
+        amount_parents = [len(self.dn.get_parents(table)) <= 1 for table in tables]
+        if not all(amount_parents):
+            raise ValueError('Some tables have multiple parents, which is not supported yet.')
+
     def fit(self):
-        """Transform the data and model the database."""
+        """Transform the data and model the database.
+
+        Raises:
+            ValueError: If the provided dataset has an unsupported structure.
+        """
         data_loader = CSVDataLoader(self.meta_file_name)
         self.dn = data_loader.load_data()
-        # transform data
+
+        self._check_unsupported_dataset_structure()
+
         self.dn.transform_data()
         self.modeler = Modeler(self.dn)
         self.modeler.model_database()
         self.sampler = Sampler(self.dn, self.modeler)
 
     def sample_rows(self, table_name, num_rows):
-        """Wrapper for Sampler.sample_rows."""
+        """Sample `num_rows` rows from the given table.
+
+        Args:
+            table_name(str): Name of the table to sample from.
+            num_rows(int): Amount of rows to sample.
+        """
         if self.sampler is None:
             raise NotFittedError('SDV instance has not been fitted')
 
         return self.sampler.sample_rows(table_name, num_rows)
 
     def sample_table(self, table_name):
-        """Wrapper for Sampler.sample_table."""
+        """Samples the given table to its original size.
+
+        Args:
+            table_name (str): Table to sample.
+        """
         if self.sampler is None:
             raise NotFittedError('SDV instance has not been fitted')
 
         return self.sampler.sample_table(table_name)
 
     def sample_all(self, num_rows=5):
-        """Wrapper for Sampler.sample_all."""
+        """Sample the whole dataset.
+
+        Args:
+            num_rows (int): Amount of rows to sample.
+        """
         if self.sampler is None:
             raise NotFittedError('SDV instance has not been fitted')
+
         return self.sampler.sample_all(num_rows)
 
     def save(self, filename):
