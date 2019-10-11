d64e1b834a01d8a65b2ef6bf90d6a2ae677241cf
JDTheRipperPC <jdtheripperpc@gmail.com>
2019-10-11 16:08:03 +0200

    new tests for test_sdv and changes on test_modeler

--- tests/test_sdv.py
+++ tests/test_sdv.py
@@ -1,87 +1,160 @@
-from unittest import TestCase, mock
+from unittest import TestCase
+from unittest.mock import Mock, MagicMock, patch
+
+import pytest
 
 from copulas import NotFittedError
 
 from sdv import SDV
+from sdv.modeler import DEFAULT_MODEL
 
 
 class TestSDV(TestCase):
 
-    def test__check_unsupported_raises(self):
-        """_check_unsupported will raise a ValueError if a table has two parents."""
+    def test____init__(self):
+        """Create default instance"""
+        # Run
+        sdv = SDV()
+
+        # Asserts
+        self.assertIsNone(sdv.sampler, "Expected default sdv.sampler as None")
+        self.assertEqual(sdv.model, DEFAULT_MODEL)
+        self.assertIsNone(sdv.distribution)
+        self.assertIsNone(sdv.model_kwargs)
+
+    def test__check_unsupported_dataset_structure_no_error(self):
+        """Test that any error is raised with a supported structure"""
         # Setup
-        instance = SDV(meta_file_name='meta.json')
+        table_names = ['foo', 'bar', 'tar']
+        parents = [[], ['foo'], ['bar']]
 
-        data_navigator_mock = mock.MagicMock()
-        data_navigator_mock.tables.keys.return_value = ['A', 'B']
-        data_navigator_mock.get_parents.return_value = ['X', 'Y']
+        # Run
+        sdv = Mock()
+        sdv.metadata.get_table_names.return_value = table_names
+        sdv.metadata.get_parents.side_effect = parents
 
-        instance.dn = data_navigator_mock
+        SDV._check_unsupported_dataset_structure(sdv)
 
-        # Run / Check
+        # Asserts
+        expect_get_parents_call_count = 3
+        self.assertEqual(sdv.metadata.get_parents.call_count, expect_get_parents_call_count)
+
+    def test__check_unsupported_dataset_structure_raise_error(self):
+        """Test that a ValueError is raised because the bad structure"""
+        # Setup
+        table_names = ['foo', 'bar', 'tar']
+        parents = [[], [], ['foo', 'bar']]
+
+        # Run & assert
+        sdv = Mock()
+        sdv.metadata.get_table_names.return_value = table_names
+        sdv.metadata.get_parents.side_effect = parents
+
         with self.assertRaises(ValueError):
-            instance._check_unsupported_dataset_structure()
+            SDV._check_unsupported_dataset_structure(sdv)
 
-    def test_sample_rows_not_fitted(self):
-        """Check that the sample_rows raise an exception when is not fitted."""
+    @patch('sdv.sdv.Sampler')
+    @patch('sdv.sdv.Modeler')
+    @patch('sdv.sdv.Metadata')
+    def test_fit_without_root_path(self, mock_metadata, mock_modeler, mock_sampler):
+        """Test fit without the root_path argument"""
+        # Run
+        metadata = '/path/to/metadata'
 
-        # Setup
+        sdv_mock = Mock()
+        sdv_mock.model = None
+        sdv_mock.distribution = None
+        sdv_mock.model_kwargs = None
 
-        # Run and asserts
-        sdv_mock = mock.Mock()
-        sdv_mock.sampler = None
+        SDV.fit(sdv_mock, metadata)
 
-        table_name = 'DEMO'
-        num_rows = 5
-
-        with self.assertRaises(NotFittedError):
-            SDV.sample_rows(sdv_mock, table_name, num_rows)
+        # Asserts
+        mock_metadata.assert_called_once_with(
+            '/path/to/metadata',
+            None
+        )
+        mock_modeler.assert_called_once_with(
+            metadata=mock_metadata.return_value,
+            model=None,
+            distribution=None,
+            model_kwargs=None
+        )
+        mock_sampler.assert_called_once_with(
+            mock_metadata.return_value,
+            mock_modeler.return_value
+        )
 
     def test_sample_rows_fitted(self):
         """Check that the sample_rows is called."""
-
-        # Setup
-
         # Run
-        sdv_mock = mock.Mock()
-        sdv_mock.sampler = mock.Mock()
-
+        sdv = Mock()
         table_name = 'DEMO'
         num_rows = 5
 
-        SDV.sample_rows(sdv_mock, table_name, num_rows)
+        SDV.sample_rows(sdv, table_name, num_rows)
 
         # Asserts
-        sdv_mock.sampler.sample_rows.assert_called_once_with(
+        sdv.sampler.sample_rows.assert_called_once_with(
             'DEMO', 5, sample_children=True, reset_primary_keys=False)
+
+    def test_sample_rows_not_fitted(self):
+        """Check that the sample_rows raise an exception when is not fitted."""
+        # Run and asserts
+        sdv = Mock()
+        sdv.sampler = None
+        table_name = 'DEMO'
+        num_rows = 5
+
+        with self.assertRaises(NotFittedError):
+            SDV.sample_rows(sdv, table_name, num_rows)
+
+    def test_sample_table_fitted(self):
+        """Check that the sample_table is called"""
+        # Run
+        sdv = Mock()
+        table_name = 'DEMO'
+
+        SDV.sample_table(sdv, table_name)
+
+        # Asserts
+        sdv.sampler.sample_table.assert_called_once_with(
+            'DEMO', None, reset_primary_keys=False)
 
     def test_sample_table_not_fitted(self):
         """Check that the sample_table raise an exception when is not fitted."""
-
-        # Setup
-
         # Run and asserts
-        sdv_mock = mock.Mock()
-        sdv_mock.sampler = None
-
+        sdv = Mock()
+        sdv.sampler = None
         table_name = 'DEMO'
 
         with self.assertRaises(NotFittedError):
-            SDV.sample_table(sdv_mock, table_name)
+            SDV.sample_table(sdv, table_name)
 
-    def test_sample_table_fitted(self):
-        """Check that the sample_table is called."""
-
-        # Setup
-
+    def test_sample_all_fitted(self):
+        """Check that the sample_all is called"""
         # Run
-        sdv_mock = mock.Mock()
-        sdv_mock.sampler = mock.Mock()
+        sdv = Mock()
 
-        table_name = 'DEMO'
-
-        SDV.sample_table(sdv_mock, table_name)
+        SDV.sample_all(sdv)
 
         # Asserts
-        sdv_mock.sampler.sample_table.assert_called_once_with(
-            'DEMO', reset_primary_keys=False)
+        sdv.sampler.sample_all.assert_called_once_with(5, reset_primary_keys=False)
+
+    def test_sample_all_not_fitted(self):
+        """Check that the sample_all raise an exception when is not fitted."""
+        # Run & asserts
+        sdv = Mock()
+        sdv.sampler = None
+
+        with self.assertRaises(NotFittedError):
+            SDV.sample_all(sdv)
+
+    @pytest.mark.skip(reason="currently not implemented")
+    def test_save(self):
+        """Test save SDV instance"""
+        pass
+
+    @pytest.mark.skip(reason="currently not implemented")
+    def test_load(self):
+        """Test load SDV instance"""
+        pass
