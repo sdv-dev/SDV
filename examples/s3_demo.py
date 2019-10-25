91276bfcad78f8009cdb7838b03dc483cf834f6c
Carles Sala <carles@pythiac.com>
2019-10-25 09:35:11 -0400

    Reorganize examples

--- examples/s3_demo.py
+++ examples/s3_demo.py
@@ -1,27 +1,49 @@
+import io
 import logging
 import os
 import sys
+import urllib
+import zipfile
 from timeit import default_timer as timer
 
-from examples.utils import download_folder
 from sdv.sdv import SDV
 
+LOGGER = logging.getLogger(__name__)
 
-def get_logger():
-    logger = logging.getLogger()
-    # We create a formatter
-    fmt = '%(asctime)s - %(process)d - %(module)s - %(levelname)s - %(message)s'
-    formatter = logging.Formatter(fmt)
-
-    # Setup Handler
-    console_handler = logging.StreamHandler()
-    logger.setLevel(logging.INFO)
-    console_handler.setFormatter(formatter)
-    logger.addHandler(console_handler)
-    return logger
+BUCKET_NAME = 'hdi-demos'
+SDV_NAME = 'sdv-demo'
+SUFFIX = '.zip'
 
 
-LOGGER = get_logger()
+def download_folder(folder_name):
+    """Downloads and extracts demo folder from S3"""
+    zip_name = folder_name + SUFFIX
+    key = os.path.join(SDV_NAME, zip_name)
+
+    # If the directory doesn't exist , we create it
+    # If it exists, we check for the folder_name for early exit
+    if not os.path.exists('demo'):
+        os.makedirs('demo')
+
+    else:
+        if os.path.exists(os.path.join('demo', folder_name)):
+            return
+
+    # try to download files from s3
+    try:
+        url = 'https://{}.s3.amazonaws.com/{}'.format(BUCKET_NAME, key)
+        response = urllib.request.urlopen(url)
+        bytes_io = io.BytesIO(response.read())
+
+    except urllib.error.HTTPError as error:
+        if error.code == 404:
+            LOGGER.error('File %s not found.', key)
+        raise
+
+    # unzip folder
+    zip_ref = zipfile.ZipFile(bytes_io, 'r')
+    zip_ref.extractall('demo')
+    zip_ref.close()
 
 
 table_dict = {
@@ -64,5 +86,7 @@ def run_demo(folder_name):
 
 
 if __name__ == '__main__':
+    fmt = '%(asctime)s - %(process)d - %(module)s - %(levelname)s - %(message)s'
+    logging.basicConfig(level=logging.INFO, format=fmt)
     download_folder(sys.argv[1])
     run_demo(sys.argv[1])
