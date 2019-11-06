1cf4c2ba779ce9ee79f2546c014dcff16d8aedd2
Carles Sala <carles@pythiac.com>
2019-11-05 23:26:11 -0500

    Bump version: 0.2.0.dev0 → 0.2.0.dev1

--- setup.py
+++ setup.py
@@ -90,6 +90,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/HDI-Project/SDV',
-    version='0.2.0.dev0',
+    version='0.2.0.dev1',
     zip_safe=False,
 )
