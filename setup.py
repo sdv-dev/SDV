812a14eb31085b0281c68a60fcd0f6bc0e68fede
JDTheRipperPC <jdtheripperpc@gmail.com>
2019-12-23 18:15:15 +0100

    Bump version: 0.3.0 → 0.3.1.dev0

--- setup.py
+++ setup.py
@@ -90,6 +90,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/HDI-Project/SDV',
-    version='0.3.0',
+    version='0.3.1.dev0',
     zip_safe=False,
 )
