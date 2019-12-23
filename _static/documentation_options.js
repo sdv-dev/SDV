6d1d9b8314ff4246de3d1ce0dd493596863a6fb9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-23 16:54:38 +0000

    Deploy HDI-Project/SDV to github.com/HDI-Project/SDV.git:gh-pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -3,6 +3,7 @@ var DOCUMENTATION_OPTIONS = {
     VERSION: '0.2.3.dev0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
+    BUILDER: 'html',
     FILE_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
