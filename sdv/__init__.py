859cd46c0cb75c6611bc5ef8b3a70f3f930fd963
Carles Sala <carles@pythiac.com>
2020-06-26 20:22:15 +0200

    Bump version: 0.3.3.dev0 → 0.3.3

--- sdv/__init__.py
+++ sdv/__init__.py
@@ -6,7 +6,7 @@
 
 __author__ = """MIT Data To AI Lab"""
 __email__ = 'dailabmit@gmail.com'
-__version__ = '0.3.3.dev0'
+__version__ = '0.3.3'
 
 from sdv.demo import get_available_demos, load_demo
 from sdv.metadata import Metadata
