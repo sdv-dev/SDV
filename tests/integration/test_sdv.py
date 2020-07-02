9753a217955bfc222d925b06bef2ab5cdbf68d4a
Carles Sala <carles@pythiac.com>
2020-07-02 20:04:28 +0200

    Add integration test for multi-parent dataset

--- tests/integration/test_sdv.py
+++ tests/integration/test_sdv.py
@@ -34,3 +34,39 @@ def test_sdv():
 
     assert transactions.shape == tables['transactions'].shape
     assert set(transactions.columns) == set(tables['transactions'].columns)
+
+
+def test_sdv_multiparent():
+    metadata, tables = load_demo('got_families', metadata=True)
+
+    sdv = SDV()
+    sdv.fit(metadata, tables)
+
+    # Sample all
+    sampled = sdv.sample_all()
+
+    assert set(sampled.keys()) == {'characters', 'families', 'character_families'}
+    assert len(sampled['characters']) == 7
+
+    # Sample with children
+    sampled = sdv.sample('characters', reset_primary_keys=True)
+
+    assert set(sampled.keys()) == {'characters', 'character_families'}
+    assert len(sampled['characters']) == 7
+    assert 'family_id' in sampled['character_families']
+
+    # Sample without children
+    characters = sdv.sample('characters', sample_children=False)
+
+    assert characters.shape == tables['characters'].shape
+    assert set(characters.columns) == set(tables['characters'].columns)
+
+    families = sdv.sample('families', sample_children=False)
+
+    assert families.shape == tables['families'].shape
+    assert set(families.columns) == set(tables['families'].columns)
+
+    character_families = sdv.sample('character_families', sample_children=False)
+
+    assert character_families.shape == tables['character_families'].shape
+    assert set(character_families.columns) == set(tables['character_families'].columns)
