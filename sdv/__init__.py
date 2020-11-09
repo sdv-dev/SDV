19c79da3684cf7d68d603eb868ae62a671803a96
Carles Sala <carles@pythiac.com>
2020-11-09 19:18:57 +0100

    Bump version: 0.4.6.dev1 → 0.4.6.dev2

--- sdv/__init__.py
+++ sdv/__init__.py
@@ -6,7 +6,7 @@
 
 __author__ = """MIT Data To AI Lab"""
 __email__ = 'dailabmit@gmail.com'
-__version__ = '0.4.6.dev1'
+__version__ = '0.4.6.dev2'
 
 from sdv import constraints, evaluation, metadata, relational, tabular
 from sdv.demo import get_available_demos, load_demo
