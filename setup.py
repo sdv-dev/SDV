bea2300ffc04d8faa8f05bff19cb7d4899933c35
Carles Sala <carles@pythiac.com>
2020-11-20 13:35:04 +0100

    Upgrade copulas

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ install_requires = [
     'numpy>=1.17.4,<2',
     'pandas>=1.1,<2',
     'graphviz>=0.13.2,<1',
-    'copulas>=0.3.2,<0.4',
+    'copulas>=0.3.3,<0.4',
     'rdt>=0.2.8.dev0,<0.3',
     'sdmetrics>=0.0.3.dev0,<0.1.0',
     'deepecho==0.1.3',
