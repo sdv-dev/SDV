de8c14c3a5097ffaa4f4b2cdc950962bcb5b2755
Carles Sala <carles@pythiac.com>
2020-11-23 15:45:33 +0100

    Use final rdt and sdmetrics versions

--- setup.py
+++ setup.py
@@ -18,8 +18,8 @@ install_requires = [
     'pandas>=1.1,<2',
     'graphviz>=0.13.2,<1',
     'copulas>=0.3.3,<0.4',
-    'rdt>=0.2.8.dev0,<0.3',
-    'sdmetrics>=0.0.3.dev0,<0.1.0',
+    'rdt>=0.2.8,<0.3',
+    'sdmetrics>=0.0.3,<0.1.0',
     'deepecho==0.1.3',
 ]
 
