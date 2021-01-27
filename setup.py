79052bcf664fa63cd692fcc24e72eab2e5262cc1
Carles Sala <carles@pythiac.com>
2021-01-28 00:40:28 +0100

    Bump version: 0.6.2.dev3 → 0.7.0.dev0

--- setup.py
+++ setup.py
@@ -98,6 +98,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/sdv-dev/SDV',
-    version='0.6.2.dev3',
+    version='0.7.0.dev0',
     zip_safe=False,
 )
