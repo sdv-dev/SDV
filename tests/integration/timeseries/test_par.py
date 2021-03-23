aa1ce100dde834be4e0560777534b9b635831fd1
Felipe Hofmann <fealho@gmail.com>
2021-03-22 18:57:08 -0700

    Add test

--- tests/integration/timeseries/test_par.py
+++ tests/integration/timeseries/test_par.py
@@ -3,6 +3,8 @@ from deepecho import load_demo
 
 from sdv.timeseries.deepecho import PAR
 
+import datetime
+
 
 def test_par():
     data = load_demo()
@@ -47,3 +49,15 @@ def test_par():
     assert sampled.shape == data.shape
     assert (sampled.dtypes == data.dtypes).all()
     assert (sampled.notnull().sum(axis=1) != 0).all()
+
+def test_column_after_date():
+    """Test that adding columns after the `sequence_index` column works."""
+    date = datetime.datetime.strptime('2020-01-01', '%Y-%m-%d')
+    daily_timeseries = pd.DataFrame({
+        'col': ['a', 'a'],
+        'date': [date, date],
+        'col2': ['hello', 'world'],
+    })
+
+    model = PAR(entity_columns=['col'], sequence_index='date')
+    model.fit(daily_timeseries)
