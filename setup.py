53e2781565e025b603e781bea8fbc3d1b2f1214e
amontane <andrew@MacBook-Pro-2.attlocal.net>
2021-04-28 14:02:49 -0500

    Bump version: 0.9.1.dev0 → 0.9.1.dev1

--- setup.py
+++ setup.py
@@ -104,6 +104,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/sdv-dev/SDV',
-    version='0.9.1.dev0',
+    version='0.9.1.dev1',
     zip_safe=False,
 )
