ef4161fe0dd24156a6ee6d40896f988048d1fe6b
Neha Patki <nehapatki@gmail.com>
2021-07-12 18:40:29 -0400

    Bump version: 0.11.0.dev1 → 0.11.0

--- setup.py
+++ setup.py
@@ -107,6 +107,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/sdv-dev/SDV',
-    version='0.11.0.dev1',
+    version='0.11.0',
     zip_safe=False,
 )
