42b6f33a3bffc845d97f5e4d3eaf68384ad2a440
Felipe Hofmann <fealho@gmail.com>
2021-08-12 22:39:11 -0700

    Bump version: 0.11.1.dev0 → 0.12.0.dev0

--- setup.py
+++ setup.py
@@ -107,6 +107,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/sdv-dev/SDV',
-    version='0.11.1.dev0',
+    version='0.12.0.dev0',
     zip_safe=False,
 )
