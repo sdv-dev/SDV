86975121c7ebc8f32618c0ad613cb5d9a9d35f2a
amontanez24 <amontanez2424@gmail.com>
2021-11-16 19:53:57 -0600

    improving minimum script to handle weird versions

--- tasks.py
+++ tasks.py
@@ -61,7 +61,7 @@ def install_minimum(c):
             if _validate_python_version(line):
                 requirement = re.match(r'[^>]*', line).group(0)
                 requirement = re.sub(r"""['",]""", '', requirement)
-                version = re.search(r'>=?(\d\.?)+', line).group(0)
+                version = re.search(r'>=?[^(,|#)]*', line).group(0)
                 if version:
                     version = re.sub(r'>=?', '==', version)
                     version = re.sub(r"""['",]""", '', version)
