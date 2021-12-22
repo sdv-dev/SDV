721a04a4cf30e8bbc03968fba4b5507bec33b68a
Katharine Xiao <2405771+katxiao@users.noreply.github.com>
2021-12-22 15:33:55 -0500

    Bump version: 0.13.1.dev1 → 0.13.1

--- setup.py
+++ setup.py
@@ -112,6 +112,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/sdv-dev/SDV',
-    version='0.13.1.dev1',
+    version='0.13.1',
     zip_safe=False,
 )
