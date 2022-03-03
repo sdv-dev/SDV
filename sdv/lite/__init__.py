e4acbc04f8a1159d7272c5030f2dd008bd5fa33c
Katharine Xiao <2405771+katxiao@users.noreply.github.com>
2022-03-02 19:49:30 -0500

    Add sdv lite module

---
+++ sdv/lite/__init__.py
@@ -0,0 +1,7 @@
+"""SDV lite package that contains model presets."""
+
+from sdv.tabular import TabularPreset
+
+__all__ = (
+    'TabularPreset',
+)
---
+++ sdv/lite/tabular.py
@@ -0,0 +1,4 @@
+"""Base class for tabular model presets."""
+
+class TabularPreset:
+    """Base class for all tabular model presets."""
