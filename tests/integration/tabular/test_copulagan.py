aa88691cea0c8f155df10fbacebab17baf7f0755
Felipe Hofmann <fealho@gmail.com>
2022-05-10 08:12:14 -0700

    Remove parametric

--- tests/integration/tabular/test_copulagan.py
+++ tests/integration/tabular/test_copulagan.py
@@ -16,7 +16,7 @@ def test_copulagan():
         field_distributions={
             'age': 'beta'
         },
-        default_distribution='bounded'
+        default_distribution='beta'
     )
     model.fit(users)
 
