67614bd2c01c1064850906bf12b62386ff31b02f
amontanez24 <amontanez2424@gmail.com>
2022-05-25 15:49:46 -0500

    Bump version: 0.15.0 → 0.15.1.dev0

--- setup.py
+++ setup.py
@@ -113,6 +113,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/sdv-dev/SDV',
-    version='0.15.0',
+    version='0.15.1.dev0',
     zip_safe=False,
 )
