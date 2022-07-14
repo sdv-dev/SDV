a104ce8b3f9f6752ed54501f9620c6004f0e92f9
Plamen Valentinov Kolev <pvkdeveloper@gmx.com>
2022-07-15 01:28:00 +0200

    Move STM to its own file

---
+++ sdv/metadata/single_table.py
@@ -0,0 +1,57 @@
+"""Single Table Metadata."""
+
+import copy
+import json
+
+
+class SingleTableMetadata:
+    """Single Table Metadata class."""
+
+    SCHEMA_VERSION = 'SINGLE_TABLE_V1'
+    KEYS = ['columns', 'primary_key', 'alternate_keys', 'constraints', 'SCHEMA_VERSION']
+
+    def __init__(self):
+        self._columns = {}
+        self._primary_key = None
+        self._alternate_keys = []
+        self._constraints = []
+        self._version = self.SCHEMA_VERSION
+        self._metadata = {
+            'columns': self._columns,
+            'primary_key': self._primary_key,
+            'alternate_keys': self._alternate_keys,
+            'constraints': self._constraints,
+            'SCHEMA_VERSION': self.SCHEMA_VERSION
+        }
+
+    def to_dict(self):
+        """Return a python ``dict`` representation of the ``SingleTableMetadata``."""
+        metadata = {}
+        for key, value in self._metadata.items():
+            if value:
+                metadata[key] = value
+
+        return copy.deepcopy(metadata)
+
+    def _set_metadata_dict(self, metadata):
+        """Set a ``metadata`` dictionary to the current instance."""
+        self._metadata = {}
+        for key in self.KEYS:
+            value = copy.deepcopy(metadata.get(key))
+            if value:
+                self._metadata[key] = value
+                setattr(self, f'_{key}', value)
+            else:
+                self._metadata[key] = getattr(self, f'_{key}')
+
+    @classmethod
+    def _load_from_dict(cls, metadata):
+        """Create a ``SingleTableMetadata`` instance from a python ``dict``."""
+        instance = cls()
+        instance._set_metadata_dict(metadata)
+        return instance
+
+    def __repr__(self):
+        """Pretty print the ``SingleTableMetadata```SingleTableMetadata``."""
+        printed = json.dumps(self.to_dict(), indent=4)
+        return printed
---
+++ tests/unit/metadata/test_single_table.py
@@ -0,0 +1,104 @@
+"""Test Single Table Metadata."""
+
+from sdv.metadata.single_table import SingleTableMetadata
+
+
+class TestSingleTableMetadata:
+    """Test ``SingleTableMetadata`` class."""
+
+    def test___init__(self):
+        """Test creating an instance of ``SingleTableMetadata``."""
+        # Run
+        instance = SingleTableMetadata()
+
+        # Assert
+        assert instance._columns == {}
+        assert instance._primary_key is None
+        assert instance._alternate_keys == []
+        assert instance._constraints == []
+        assert instance._version == 'SINGLE_TABLE_V1'
+        assert instance._metadata == {
+            'columns': {},
+            'primary_key': None,
+            'alternate_keys': [],
+            'constraints': [],
+            'SCHEMA_VERSION': 'SINGLE_TABLE_V1'
+        }
+
+    def test_to_dict(self):
+        """Test the ``to_dict`` method from ``SingleTableMetadata``.
+
+        Setup:
+            - Instance of ``SingleTableMetadata`` and modify the ``instance._columns`` to ensure
+            that ``to_dict`` works properly.
+        Output:
+            - A dictionary representation of the ``instance`` that does not modify the
+              internal dictionaries.
+        """
+        # Setup
+        instance = SingleTableMetadata()
+        instance._columns['my_column'] = 'value'
+
+        # Run
+        result = instance.to_dict()
+
+        # Assert
+        assert result == {
+            'columns': {'my_column': 'value'},
+            'SCHEMA_VERSION': 'SINGLE_TABLE_V1'
+        }
+
+        result['columns']['my_column'] = 1
+        assert instance._columns['my_column'] == 'value'
+
+    def test__set_metadata_dict(self):
+        """Test the ``_set_metadata_dict`` to a instance.
+
+        Setup:
+            - Instance of ``SingleTableMetadata``.
+            - Dictionary representing ``SingleTableMetadata``.
+
+        Output:
+            - ``SingleTableMetadata`` instance with the dictionary represented values.
+        """
+        # Setup
+        instance = SingleTableMetadata()
+        metadata = {
+            'columns': {'my_column': 'value'},
+            'SCHEMA_VERSION': 'SINGLE_TABLE_V1'
+        }
+
+        # Run
+        instance._set_metadata_dict(metadata)
+
+        # Assert
+        assert instance._metadata == {
+            'columns': {'my_column': 'value'},
+            'primary_key': None,
+            'alternate_keys': [],
+            'constraints': [],
+            'SCHEMA_VERSION': 'SINGLE_TABLE_V1'
+        }
+
+        assert instance._columns == {'my_column': 'value'}
+
+    def test__load_from_dict(self):
+        """Test that ``_load_from_dict`` returns a instance with the ``dict`` updated objects."""
+        # Setup
+        my_metadata = {
+            'columns': {'my_column': 'value'},
+            'primary_key': 'pk',
+            'alternate_keys': [],
+            'constraints': [],
+            'SCHEMA_VERSION': 'SINGLE_TABLE_V1'
+        }
+
+        # Run
+        instance = SingleTableMetadata._load_from_dict(my_metadata)
+
+        # Assert
+        assert instance._metadata == my_metadata
+        assert instance._columns == {'my_column': 'value'}
+        assert instance._primary_key == 'pk'
+        assert instance._alternate_keys == []
+        assert instance._constraints == []
