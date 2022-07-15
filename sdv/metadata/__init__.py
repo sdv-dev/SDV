2d76f4dd114a014b3a75d333f822e674a13e652a
Plamen Valentinov Kolev <41479552+pvk-developer@users.noreply.github.com>
2022-07-15 21:50:57 +0200

    Create SingleTableMetadata class  (#906)
    * SingleTableMetadata implementation
    
    * Move STM to its own file
    
    * Add integration tests
    
    * bump the coverage
    
    * Fix isort
    
    * Address comments

--- sdv/metadata/__init__.py
+++ sdv/metadata/__init__.py
@@ -3,12 +3,14 @@
 from sdv.metadata import visualization
 from sdv.metadata.dataset import Metadata
 from sdv.metadata.errors import MetadataError, MetadataNotFittedError
+from sdv.metadata.single_table import SingleTableMetadata
 from sdv.metadata.table import Table
 
 __all__ = (
     'Metadata',
     'MetadataError',
     'MetadataNotFittedError',
+    'SingleTableMetadata',
     'Table',
     'visualization'
 )
