ba50abdfc90b59178da4cde1813c9082f455a1d2
Plamen Valentinov Kolev <41479552+pvk-developer@users.noreply.github.com>
2022-07-19 19:14:27 +0200

    Add detect method to `SingleTableMetadata` (#909)
    * Implement detect methods.
    
    * Add unit tests
    
    * Default to categorical if dtype not found

--- sdv/metadata/single_table.py
+++ sdv/metadata/single_table.py
@@ -2,13 +2,23 @@
 
 import copy
 import json
+from pathlib import Path
+
+import pandas as pd
 
 
 class SingleTableMetadata:
     """Single Table Metadata class."""
 
-    SCHEMA_VERSION = 'SINGLE_TABLE_V1'
+    _DTYPES_TO_SDTYPES = {
+        'i': 'numerical',
+        'f': 'numerical',
+        'O': 'categorical',
+        'b': 'boolean',
+        'M': 'datetime',
+    }
     KEYS = ['columns', 'primary_key', 'alternate_keys', 'constraints', 'SCHEMA_VERSION']
+    SCHEMA_VERSION = 'SINGLE_TABLE_V1'
 
     def __init__(self):
         self._columns = {}
@@ -23,6 +33,52 @@ class SingleTableMetadata:
             'constraints': self._constraints,
             'SCHEMA_VERSION': self.SCHEMA_VERSION
         }
+
+    def detect_from_dataframe(self, data):
+        """Detect the metadata from a ``pd.DataFrame`` object.
+
+        This method automatically detects the ``sdtypes`` for the given ``pandas.DataFrame``.
+
+        Args:
+            data (pd.DataFrame):
+                ``pandas.DataFrame`` to detect the metadata from.
+        """
+        if self._columns:
+            raise ValueError(
+                'Metadata already exists. Create a new ``SingleTableMetadata`` '
+                'object to detect from other data sources.'
+            )
+
+        for field in data:
+            clean_data = data[field].dropna()
+            kind = clean_data.infer_objects().dtype.kind
+            self._columns[field] = {'sdtype': self._DTYPES_TO_SDTYPES.get(kind, 'categorical')}
+
+        print('Detected metadata:')
+        print(json.dumps(self.to_dict(), indent=4))
+
+    def detect_from_csv(self, filepath, pandas_kwargs=None):
+        """Detect the metadata from a ``csv`` file.
+
+        This method automatically detects the ``sdtypes`` for a given ``csv`` file.
+
+        Args:
+            filepath (str):
+                String that represents the ``path`` to the ``csv`` file.
+            pandas_kwargs (dict):
+                A python dictionary of with string and value accepted by ``pandas.read_csv``
+                function. Defaults to ``None``.
+        """
+        if self._columns:
+            raise ValueError(
+                'Metadata already exists. Create a new ``SingleTableMetadata`` '
+                'object to detect from other data sources.'
+            )
+
+        filepath = Path(filepath)
+        pandas_kwargs = pandas_kwargs or {}
+        data = pd.read_csv(filepath, **pandas_kwargs)
+        self.detect_from_dataframe(data)
 
     def to_dict(self):
         """Return a python ``dict`` representation of the ``SingleTableMetadata``."""
