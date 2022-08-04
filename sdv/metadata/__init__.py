1f89b02bdfb767ad8a1e5dba30d575500c531057
Felipe Alex Hofmann <fealho@gmail.com>
2022-08-03 19:18:44 -0700

    Add `validate` to `SingleTableMetadata` (#930)
    * Add key validation methods (the methods themselves still gotta be revised)
    
    * Combined errors implementation
    
    * Finish constraints
    
    * Add silly constraint solution
    
    * Add try/catches for validation + add unit and integration tests
    
    * Change MetadataError to InvalidMetadataError
    
    * Simplify tests to one element + fix lint
    
    * Remove _metadata
    
    * Change way _metadata implemented in to_dict
    
    * Add constraint validation + integration test + change _constraints to be list of tuple
    
    * Fix error message for validate + add error integration test
    
    * Fix all constraint related error messages
    
    * Add helper _append_error to validate + fix constraint related errors
    
    * Add constraint unit test
    
    * Fix lint
    
    * change _metadata[pk] to pk
    
    * Add tests requested + addresses feedback
    
    * Address minor feedback

--- sdv/metadata/__init__.py
+++ sdv/metadata/__init__.py
@@ -2,14 +2,14 @@
 
 from sdv.metadata import visualization
 from sdv.metadata.dataset import Metadata
-from sdv.metadata.errors import MetadataError, MetadataNotFittedError
+from sdv.metadata.errors import InvalidMetadataError, MetadataNotFittedError
 from sdv.metadata.multi_table import MultiTableMetadata
 from sdv.metadata.single_table import SingleTableMetadata
 from sdv.metadata.table import Table
 
 __all__ = (
     'Metadata',
-    'MetadataError',
+    'InvalidMetadataError',
     'MetadataNotFittedError',
     'MultiTableMetadata',
     'SingleTableMetadata',
