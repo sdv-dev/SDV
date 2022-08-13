0e393e1a1591291ab0b7277cc06de9e5ba08924f
Andrew Montanez <amontanez2424@gmail.com>
2022-08-12 19:51:55 -0500

    Create DataProcessor class

---
+++ sdv/data_processing/data_processor.py
@@ -0,0 +1,70 @@
+"""Single table data processing."""
+
+import rdt
+
+from sdv.constraints import Constraint
+
+
+class DataProcessor:
+    """Single table data processor.
+
+    This class handles all pre and post processing that is done to a single table to get it ready
+    for modeling and finalize sampling. These processes include formatting, transformations,
+    anonymization and constraint hanlding.
+
+    Args:
+        metadata (metadata.SingleTableMetadata):
+            The single table metadata instance that will be used to apply constraints and
+            transformations to the data.
+        learn_rounding_scheme (bool):
+            Define rounding scheme for FloatFormatter. If True, the data returned by
+            reverse_transform will be rounded to that place. Defaults to True.
+        enforce_min_max_values (bool):
+            Specify whether or not to clip the data returned by reverse_transform of the numerical
+            transformer, FloatFormatter, to the min and max values seen during fit.
+            Defaults to True.
+        model_kwargs (dict): Dictionary specifying the kwargs that need to be used in each tabular
+            model when working on this table. This dictionary contains as keys the name of the
+            TabularModel class and as values a dictionary containing the keyword arguments to use.
+            This argument exists mostly to ensure that the models are fitted using the same
+            arguments when the same DataProcessor is used to fit different model instances on
+            different slices of the same table.
+    """
+
+    _DEFAULT_TRANSFORMERS_BY_SDTYPE = {
+        'numerical': rdt.transformers.FloatFormatter(
+            learn_rounding_scheme=True,
+            enforce_min_max_values=True,
+            missing_value_replacement='mean',
+            model_missing_values=True,
+        ),
+        'categorical': rdt.transformers.LabelEncoder(add_noise=True),
+        'boolean': rdt.transformers.LabelEncoder(add_noise=True),
+        'datetime': rdt.transformers.UnixTimestampEncoder(
+            missing_value_replacement='mean',
+            model_missing_values=True,
+        )
+    }
+
+    def _load_constraints(self):
+        constraints = self.metadata._constraints or []
+        loaded_constraints = [Constraint.from_dict(constraint) for constraint in constraints]
+        return loaded_constraints
+
+    def _update_numerical_transformer(self, learn_rounding_scheme, enforce_min_max_values):
+        custom_float_formatter = rdt.transformers.FloatFormatter(
+            missing_value_replacement='mean',
+            model_missing_values=True,
+            learn_rounding_scheme=learn_rounding_scheme,
+            enforce_min_max_values=enforce_min_max_values
+        )
+        self._transformers_by_sdtype.update({'numerical': custom_float_formatter})
+
+    def __init__(self, metadata, learn_rounding_scheme=True, enforce_min_max_values=True,
+                 model_kwargs=None):
+        self.metadata = metadata
+        self._model_kwargs = model_kwargs or {}
+        self._constraints = self._load_constraints()
+        self._constraints_to_reverse = []
+        self._transformers_by_sdtype = self._DEFAULT_TRANSFORMERS_BY_SDTYPE.copy()
+        self._update_numerical_transformer(learn_rounding_scheme, enforce_min_max_values)
