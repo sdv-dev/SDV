e836839cc9bed6db22f90f8f754fe574c8d05cc5
Felipe Hofmann <fealho@gmail.com>
2022-08-18 21:06:58 -0700

    .

--- sdv/constraints/__init__.py
+++ sdv/constraints/__init__.py
@@ -1,5 +1,6 @@
 """SDV Constraints module."""
 
+from sdv.constraints.base import Constraint
 from sdv.constraints.tabular import (
     FixedCombinations, FixedIncrements, Inequality, Negative, OneHotEncoding, Positive, Range,
     ScalarInequality, ScalarRange, Unique, create_custom_constraint)
