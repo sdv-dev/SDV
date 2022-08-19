0d4278e68a7f2a134a8381c3969377e8a95311db
Felipe Hofmann <fealho@gmail.com>
2022-08-18 19:17:40 -0700

    Fix wrong conversion

--- sdv/constraints/__init__.py
+++ sdv/constraints/__init__.py
@@ -1,13 +1,12 @@
 """SDV Constraints module."""
 
-from sdv.constraints.base import Constraint
 from sdv.constraints.tabular import (
     FixedCombinations, FixedIncrements, Inequality, Negative, OneHotEncoding, Positive, Range,
     ScalarInequality, ScalarRange, Unique, create_custom_constraint)
 
 __all__ = [
     'create_custom_constraint',
-    'constraint_name',
+    'Constraint',
     'Inequality',
     'ScalarInequality',
     'FixedCombinations',
