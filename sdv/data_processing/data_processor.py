4882d0fc727ad731fd540c6d05cac0607ca6da48
Felipe Hofmann <fealho@gmail.com>
2022-08-20 13:57:37 -0700

    Add tests + methods

--- sdv/data_processing/data_processor.py
+++ sdv/data_processing/data_processor.py
@@ -1,5 +1,8 @@
 """Single table data processing."""
 
+import copy
+import json
+
 import rdt
 
 from sdv.constraints import Constraint
@@ -69,3 +72,75 @@ class DataProcessor:
         self._constraints_to_reverse = []
         self._transformers_by_sdtype = self._DEFAULT_TRANSFORMERS_BY_SDTYPE.copy()
         self._update_numerical_transformer(learn_rounding_scheme, enforce_min_max_values)
+
+    def filter_valid(self, data):
+        """Filter the data using the constraints and return only the valid rows.
+
+        Args:
+            data (pandas.DataFrame):
+                Table data.
+
+        Returns:
+            pandas.DataFrame:
+                Table containing only the valid rows.
+        """
+        for constraint in self._constraints:
+            data = constraint.filter_valid(data)
+
+        return data
+
+    def to_dict(self):
+        """Get a dict representation of this DataProcessor.
+
+        Returns:
+            dict:
+                Dict representation of this DataProcessor.
+        """
+        return {
+            'metadata': copy.deepcopy(self.metadata),
+            'transformers_by_sdtype': copy.deepcopy(self._transformers_by_sdtype),
+            'constraints_to_reverse': copy.deepcopy(self._constraints_to_reverse),
+            'model_kwargs': copy.deepcopy(self._model_kwargs)
+        }
+
+    @classmethod
+    def from_dict(cls, metadata_dict):
+        """Load a DataProcessor from a metadata dict.
+
+        Args:
+            metadata_dict (dict):
+                Dict metadata to load.
+        """
+        instance = cls(
+            metadata=metadata_dict['metadata'],
+            learn_rounding_scheme=metadata_dict.get('learn_rounding_scheme', True),
+            enforce_min_max_values=metadata_dict.get('enforce_min_max_values', True),
+            model_kwargs=metadata_dict.get('model_kwargs')
+        )
+        instance._constraints_to_reverse = metadata_dict.get('constraints_to_reverse', [])
+        instance._transformers_by_sdtype = metadata_dict.get(
+            'transformers_by_sdtype', instance._transformers_by_sdtype
+        )
+
+        return instance
+
+    def to_json(self, path):
+        """Dump this DataProcessor into a JSON file.
+
+        Args:
+            path (str):
+                Path of the JSON file where this metadata will be stored.
+        """
+        with open(path, 'w') as out_file:
+            json.dump(self.to_dict(), out_file, indent=4)
+
+    @classmethod
+    def from_json(cls, path):
+        """Load a DataProcessor from a JSON.
+
+        Args:
+            path (str):
+                Path of the JSON file to load
+        """
+        with open(path, 'r') as in_file:
+            return cls.from_dict(json.load(in_file))
