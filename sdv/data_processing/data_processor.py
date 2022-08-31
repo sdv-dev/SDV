1ba03ba8ddd59ef900d03f7eb6d94c0476e23ebc
Felipe Alex Hofmann <fealho@gmail.com>
2022-08-31 04:39:51 -0700

    Add utility methods (#965)
    * Add tests + methods
    
    * Update to_json/from_json + test cases
    
    * Address feedback

--- sdv/data_processing/data_processor.py
+++ sdv/data_processing/data_processor.py
@@ -1,8 +1,12 @@
 """Single table data processing."""
 
+import copy
+import json
+
 import rdt
 
 from sdv.constraints import Constraint
+from sdv.metadata.single_table import SingleTableMetadata
 
 
 class DataProcessor:
@@ -69,3 +73,78 @@ class DataProcessor:
         self._constraints_to_reverse = []
         self._transformers_by_sdtype = self._DEFAULT_TRANSFORMERS_BY_SDTYPE.copy()
         self._update_numerical_transformer(learn_rounding_scheme, enforce_min_max_values)
+
+    def filter_valid(self, data):
+        """Filter the data using the constraints and return only the valid rows.
+
+        Args:
+            data (pandas.DataFrame):
+                Table data.
+
+        Returns:
+            pandas.DataFrame:
+                Table containing only the valid rows.
+        """
+        for constraint in self._constraints:
+            data = constraint.filter_valid(data)
+
+        return data
+
+    def to_dict(self):
+        """Get a dict representation of this DataProcessor.
+
+        Returns:
+            dict:
+                Dict representation of this DataProcessor.
+        """
+        constraints_to_reverse = [cnt.to_dict() for cnt in self._constraints_to_reverse]
+        return {
+            'metadata': copy.deepcopy(self.metadata.to_dict()),
+            'constraints_to_reverse': constraints_to_reverse,
+            'model_kwargs': copy.deepcopy(self._model_kwargs)
+        }
+
+    @classmethod
+    def from_dict(cls, metadata_dict, learn_rounding_scheme=True, enforce_min_max_values=True):
+        """Load a DataProcessor from a metadata dict.
+
+        Args:
+            metadata_dict (dict):
+                Dict metadata to load.
+            learn_rounding_scheme (bool):
+                If passed, set the ``learn_rounding_scheme`` on the new instance.
+            enforce_min_max_values (bool):
+                If passed, set the ``enforce_min_max_values`` on the new instance.
+        """
+        instance = cls(
+            metadata=SingleTableMetadata.from_dict(metadata_dict['metadata']),
+            learn_rounding_scheme=learn_rounding_scheme,
+            enforce_min_max_values=enforce_min_max_values,
+            model_kwargs=metadata_dict.get('model_kwargs')
+        )
+        instance._constraints_to_reverse = [
+            Constraint.from_dict(cnt) for cnt in metadata_dict.get('constraints_to_reverse', [])
+        ]
+
+        return instance
+
+    def to_json(self, path):
+        """Dump this DataProcessor into a JSON file.
+
+        Args:
+            path (str):
+                Path of the JSON file where this metadata will be stored.
+        """
+        with open(path, 'w') as out_file:
+            json.dump(self.to_dict(), out_file, indent=4)
+
+    @classmethod
+    def from_json(cls, path):
+        """Load a DataProcessor from a JSON.
+
+        Args:
+            path (str):
+                Path of the JSON file to load
+        """
+        with open(path, 'r') as in_file:
+            return cls.from_dict(json.load(in_file))
