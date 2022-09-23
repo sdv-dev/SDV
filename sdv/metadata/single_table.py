84852c7262a1d47bb3f02476baf2e901768e2f8c
Andrew Montanez <andrew@sdv.dev>
2022-09-22 19:07:54 -0500

    Revert "initial commit"
    This reverts commit b30fd47d0b78f6d31e3123b48376110c9e4935b1.

--- sdv/metadata/single_table.py
+++ sdv/metadata/single_table.py
@@ -163,11 +163,6 @@ class SingleTableMetadata:
             raise ValueError(f"Please provide a 'sdtype' for column '{column_name}'.")
 
         self._validate_column(column_name, **kwargs)
-        column_kwargs = deepcopy(kwargs)
-        if sdtype not in self._EXPECTED_KWARGS:
-            pii = column_kwargs.get('pii', True)
-            column_kwargs['pii'] = pii
-
         self._columns[column_name] = deepcopy(kwargs)
 
     def _validate_column_exists(self, column_name):
@@ -285,14 +280,12 @@ class SingleTableMetadata:
 
     def _validate_keys_sdtype(self, keys, key_type):
         """Validate that no key is of type 'categorical'."""
-        bad_sdtypes = ('boolean', 'categorical')
         categorical_keys = sorted(
-            {key for key in keys if self._columns[key]['sdtype'] in bad_sdtypes}
+            {key for key in keys if self._columns[key]['sdtype'] == 'categorical'}
         )
         if categorical_keys:
             raise ValueError(
-                f"The {key_type}_keys {categorical_keys} cannot be type 'categorical' or "
-                "'boolean'."
+                f"The {key_type}_keys {categorical_keys} cannot be type 'categorical'."
             )
 
     def _validate_key(self, column_name, key_type):
@@ -421,7 +414,6 @@ class SingleTableMetadata:
         # Validate constraints
         errors = []
         for constraint_dict in self._constraints:
-            constraint_dict = deepcopy(constraint_dict)
             constraint_name = constraint_dict.pop('constraint_name')
             try:
                 self._validate_constraint(constraint_name, **constraint_dict)
