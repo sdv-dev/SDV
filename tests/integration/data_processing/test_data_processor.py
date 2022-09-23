8cff95d40ac519d7aabf2e18ce25ec7967e1be91
Plamen Valentinov Kolev <pvkdeveloper@gmx.com>
2022-09-23 10:58:40 +0200

    Update docstring

--- tests/integration/data_processing/test_data_processor.py
+++ tests/integration/data_processing/test_data_processor.py
@@ -24,7 +24,7 @@ def test_data_processor_with_anonymized_columns(tmpdir):
         - Transform the data.
         - Reverse transform the data.
 
-    Side effects:
+    Assert:
         - The column ``occupation`` has been dropped in the ``transform`` process.
         - The column ``occupation`` has been re-created with new values from the
           ``AnonymizedFaker`` instance.
@@ -69,7 +69,7 @@ def test_data_processor_with_anonymized_columns_and_primary_key(tmpdir):
         - Load metadata and data.
         - Parse the old metadata to a new metadata.
         - Anonymize the field ``occupation``.
-        - Create ``id`` and ``ssn`` columns.
+        - Create ``id``.
 
     Run:
         - Create a ``DataProcessor`` with the new metadata.
@@ -77,12 +77,11 @@ def test_data_processor_with_anonymized_columns_and_primary_key(tmpdir):
         - Transform the data.
         - Reverse transform the data.
 
-    Side effects:
+    Assert:
         - The column ``occupation`` has been dropped in the ``transform`` process.
         - The column ``occupation`` has been re-created with new values from the
           ``AnonymizedFaker`` instance.
         - The column ``id`` has been created in ``transform`` with a unique length of the data.
-        - The column ``ssn`` has been created in ``transform`` with a unique length of the data.
     """
     # Load metadata and data
     metadata, data = load_demo('adult', metadata=True)
@@ -123,18 +122,16 @@ def test_data_processor_with_anonymized_columns_and_primary_key(tmpdir):
 
 
 def test_data_processor_with_primary_key_numerical(tmpdir):
-    """Test the ``DataProcessor``.
+    """End to end test for the ``DataProcessor``.
 
-    Test that when we update a ``pii`` column this uses ``AnonymizedFaker`` to create
-    a new set of data for that column, while it drops it on the ``transform`` and this values
-    can be repeated. Meanwhile, when we set a primary key this has to have a unique length
-    of the input data.
+    Test that when running the ``DataProcessor`` with a numerical primary key, this is able
+    to generate such a key in the ``transform`` method. The key should be the same length
+    as the input data size.
 
     Setup:
         - Load metadata and data.
-        - Parse the old metadata to a new metadata.
-        - Anonymize the field ``occupation``.
-        - Create ``id`` and ``ssn`` columns.
+        - Detect the metadata from the ``dataframe``.
+        - Create ``id`` column.
 
     Run:
         - Create a ``DataProcessor`` with the new metadata.
@@ -142,12 +139,9 @@ def test_data_processor_with_primary_key_numerical(tmpdir):
         - Transform the data.
         - Reverse transform the data.
 
-    Side effects:
-        - The column ``occupation`` has been dropped in the ``transform`` process.
-        - The column ``occupation`` has been re-created with new values from the
-          ``AnonymizedFaker`` instance.
-        - The column ``id`` has been created in ``transform`` with a unique length of the data.
-        - The column ``ssn`` has been created in ``transform`` with a unique length of the data.
+    Assert:
+        - The column ``id`` has been created during ``transform`` with a unique length of the data.
+          matching the original numerical ``id`` column.
     """
     # Load metadata and data
     data = load_demo('adult')['adult']
