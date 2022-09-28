d34bba8f982c875d903221a43a3118be104ace11
Plamen Valentinov Kolev <41479552+pvk-developer@users.noreply.github.com>
2022-09-28 22:45:05 +0200

    Create GaussianCopulaSynthesizer, CTGANSynthesizer, TVAESynthesizer and CopulaGANSynthesizer (#1034)
    * Add GaussianCopulaSynthesizer
    
    * Add CopulaGANSynthesizer
    
    * Bump CTGAN
    
    * Add unit tests
    
    * Address feedback
    
    * Address comments
    
    * Change distributions names

---
+++ sdv/single_table/ctgan.py
@@ -0,0 +1,166 @@
+"""Wrapper around CTGAN model."""
+
+from ctgan import CTGAN, TVAE
+
+from sdv.single_table.base import BaseSynthesizer
+
+
+class CTGANSynthesizer(BaseSynthesizer):
+    """Model wrapping ``CTGAN`` model.
+
+    Args:
+        metadata (sdv.metadata.SingleTableMetadata):
+            Single table metadata representing the data that this synthesizer will be used for.
+        enforce_min_max_values (bool):
+            Specify whether or not to clip the data returned by ``reverse_transform`` of
+            the numerical transformer, ``FloatFormatter``, to the min and max values seen
+            during ``fit``. Defaults to ``True``.
+        enforce_rounding (bool):
+            Define rounding scheme for ``numerical`` columns. If ``True``, the data returned
+            by ``reverse_transform`` will be rounded as in the original data. Defaults to ``True``.
+        embedding_dim (int):
+            Size of the random sample passed to the Generator. Defaults to 128.
+        generator_dim (tuple or list of ints):
+            Size of the output samples for each one of the Residuals. A Residual Layer
+            will be created for each one of the values provided. Defaults to (256, 256).
+        discriminator_dim (tuple or list of ints):
+            Size of the output samples for each one of the Discriminator Layers. A Linear Layer
+            will be created for each one of the values provided. Defaults to (256, 256).
+        generator_lr (float):
+            Learning rate for the generator. Defaults to 2e-4.
+        generator_decay (float):
+            Generator weight decay for the Adam Optimizer. Defaults to 1e-6.
+        discriminator_lr (float):
+            Learning rate for the discriminator. Defaults to 2e-4.
+        discriminator_decay (float):
+            Discriminator weight decay for the Adam Optimizer. Defaults to 1e-6.
+        batch_size (int):
+            Number of data samples to process in each step.
+        discriminator_steps (int):
+            Number of discriminator updates to do for each generator update.
+            From the WGAN paper: https://arxiv.org/abs/1701.07875. WGAN paper
+            default is 5. Default used is 1 to match original CTGAN implementation.
+        log_frequency (boolean):
+            Whether to use log frequency of categorical levels in conditional
+            sampling. Defaults to ``True``.
+        verbose (boolean):
+            Whether to have print statements for progress results. Defaults to ``False``.
+        epochs (int):
+            Number of training epochs. Defaults to 300.
+        pac (int):
+            Number of samples to group together when applying the discriminator.
+            Defaults to 10.
+        cuda (bool or str):
+            If ``True``, use CUDA. If a ``str``, use the indicated device.
+            If ``False``, do not use cuda at all.
+    """
+
+    _MODEL_CLASS = CTGAN
+
+    def __init__(self, metadata, enforce_min_max_values=True, enforce_rounding=True,
+                 embedding_dim=128, generator_dim=(256, 256), discriminator_dim=(256, 256),
+                 generator_lr=2e-4, generator_decay=1e-6, discriminator_lr=2e-4,
+                 discriminator_decay=1e-6, batch_size=500, discriminator_steps=1,
+                 log_frequency=True, verbose=False, epochs=300, pac=10, cuda=True):
+
+        super().__init__(
+            metadata=metadata,
+            enforce_min_max_values=enforce_min_max_values,
+            enforce_rounding=enforce_rounding,
+        )
+
+        self.embedding_dim = embedding_dim
+        self.generator_dim = generator_dim
+        self.discriminator_dim = discriminator_dim
+        self.generator_lr = generator_lr
+        self.generator_decay = generator_decay
+        self.discriminator_lr = discriminator_lr
+        self.discriminator_decay = discriminator_decay
+        self.batch_size = batch_size
+        self.discriminator_steps = discriminator_steps
+        self.log_frequency = log_frequency
+        self.verbose = verbose
+        self.epochs = epochs
+        self.pac = pac
+        self.cuda = cuda
+
+        self._model_kwargs = {
+            'embedding_dim': embedding_dim,
+            'generator_dim': generator_dim,
+            'discriminator_dim': discriminator_dim,
+            'generator_lr': generator_lr,
+            'generator_decay': generator_decay,
+            'discriminator_lr': discriminator_lr,
+            'discriminator_decay': discriminator_decay,
+            'batch_size': batch_size,
+            'discriminator_steps': discriminator_steps,
+            'log_frequency': log_frequency,
+            'verbose': verbose,
+            'epochs': epochs,
+            'pac': pac,
+            'cuda': cuda
+        }
+
+
+class TVAESynthesizer(BaseSynthesizer):
+    """Model wrapping ``TVAE`` model.
+
+    Args:
+        metadata (sdv.metadata.SingleTableMetadata):
+            Single table metadata representing the data that this synthesizer will be used for.
+        enforce_min_max_values (bool):
+            Specify whether or not to clip the data returned by ``reverse_transform`` of
+            the numerical transformer, ``FloatFormatter``, to the min and max values seen
+            during ``fit``. Defaults to ``True``.
+        enforce_rounding (bool):
+            Define rounding scheme for ``numerical`` columns. If ``True``, the data returned
+            by ``reverse_transform`` will be rounded as in the original data. Defaults to ``True``.
+        embedding_dim (int):
+            Size of the random sample passed to the Generator. Defaults to 128.
+        compress_dims (tuple or list of ints):
+            Size of each hidden layer in the encoder. Defaults to (128, 128).
+        decompress_dims (tuple or list of ints):
+           Size of each hidden layer in the decoder. Defaults to (128, 128).
+        l2scale (int):
+            Regularization term. Defaults to 1e-5.
+        batch_size (int):
+            Number of data samples to process in each step.
+        epochs (int):
+            Number of training epochs. Defaults to 300.
+        loss_factor (int):
+            Multiplier for the reconstruction error. Defaults to 2.
+        cuda (bool or str):
+            If ``True``, use CUDA. If a ``str``, use the indicated device.
+            If ``False``, do not use cuda at all.
+    """
+
+    _MODEL_CLASS = TVAE
+
+    def __init__(self, metadata, enforce_min_max_values=True, enforce_rounding=True,
+                 embedding_dim=128, compress_dims=(128, 128), decompress_dims=(128, 128),
+                 l2scale=1e-5, batch_size=500, epochs=300, loss_factor=2, cuda=True):
+
+        super().__init__(
+            metadata=metadata,
+            enforce_min_max_values=enforce_min_max_values,
+            enforce_rounding=enforce_rounding,
+        )
+        self.embedding_dim = embedding_dim
+        self.compress_dims = compress_dims
+        self.decompress_dims = decompress_dims
+        self.l2scale = l2scale
+        self.batch_size = batch_size
+        self.epochs = epochs
+        self.loss_factor = loss_factor
+        self.cuda = cuda
+
+        self._model_kwargs = {
+            'embedding_dim': embedding_dim,
+            'compress_dims': compress_dims,
+            'decompress_dims': decompress_dims,
+            'l2scale': l2scale,
+            'batch_size': batch_size,
+            'epochs': epochs,
+            'loss_factor': loss_factor,
+            'cuda': cuda
+        }
