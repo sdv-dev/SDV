890ac723cc20fa21010410dcdfdf4a510532959f
Plamen Valentinov Kolev <41479552+pvk-developer@users.noreply.github.com>
2022-09-28 22:45:05 +0200

    Create GaussianCopulaSynthesizer, CTGANSynthesizer, TVAESynthesizer and CopulaGANSynthesizer (#1034)
    * Add GaussianCopulaSynthesizer
    
    * Add CopulaGANSynthesizer
    
    * Bump CTGAN
    
    * Add unit tests
    
    * Address feedback
    
    * Address comments
    
    * Change distributions names

--- sdv/single_table/__init__.py
+++ sdv/single_table/__init__.py
@@ -1,1 +1,12 @@
-"""Models for Single Table."""
+"""Models for Single Table data."""
+
+from sdv.single_table.copulagan import CopulaGANSynthesizer
+from sdv.single_table.copulas import GaussianCopulaSynthesizer
+from sdv.single_table.ctgan import CTGANSynthesizer, TVAESynthesizer
+
+__all__ = (
+    'GaussianCopulaSynthesizer',
+    'CTGANSynthesizer',
+    'TVAESynthesizer',
+    'CopulaGANSynthesizer',
+)
