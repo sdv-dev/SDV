57cdb064c2de8663d04bb84d75e9483b0f908fcc
Plamen Valentinov Kolev <pvkdeveloper@gmx.com>
2022-11-03 19:07:56 +0100

    Add fk validation

--- sdv/multi_table/base.py
+++ sdv/multi_table/base.py
@@ -4,6 +4,7 @@ from collections import defaultdict
 from copy import deepcopy
 
 from sdv.single_table.copulas import GaussianCopulaSynthesizer
+from sdv.single_table.errors import InvalidDataError
 
 
 class BaseMultiTableSynthesizer:
@@ -82,3 +83,61 @@ class BaseMultiTableSynthesizer:
     def get_metadata(self):
         """Return the ``MultiTableMetadata`` for this synthesizer."""
         return self.metadata
+
+    def _validate_foreign_keys(self, data):
+        errors = []
+        for relation in self.metadata._relationships:
+            child_table = data[relation['child_table_name']]
+            parent_table = data[relation['parent_table_name']]
+            child_row = child_table[relation['child_foreign_key']]
+            parent_row = parent_table[relation['parent_primary_key']]
+            missing_values = child_row[~child_row.isin(parent_row)].unique()
+            if missing_values:
+                if len(missing_values) > 5:
+                    message = f'({value for value in missing_values[:5]}, + more).'
+                else:
+                    message = f'({value for value in missing_values}).'
+
+                errors.append(
+                    f"Error: foreign key column {relation['child_foreign_key']} contains "
+                    f'unknown references: {message}'
+                )
+
+        return errors
+
+    def validate(self, data):
+        """Validate data.
+
+        Args:
+            data (dict):
+                A dictionary with key as table name and ``pandas.DataFrame`` as value to validate.
+
+        Raises:
+            ValueError:
+                Raised when data is not of type pd.DataFrame.
+            InvalidDataError:
+                Raised if:
+                    * foreign key does not belong to a primay key
+                    * data columns don't match metadata
+                    * keys have missing values
+                    * primary or alternate keys are not unique
+                    * context columns vary for a sequence key
+                    * values of a column don't satisfy their sdtype
+        """
+        errors = []
+        for table_name, table_data in data.items():
+            try:
+                self._table_synthesizers[table_name].validate(table_data)
+
+            except (InvalidDataError, ValueError) as e:
+                errors.append(e)
+
+        if errors:
+            error_msg = '\n'.join(errors)
+            raise InvalidDataError(error_msg)
+
+        foreign_key_errors = self._validate_foreign_keys(data)
+
+        if foreign_key_errors:
+            error_msg = '\n'.join(foreign_key_errors)
+            raise InvalidDataError(error_msg)
