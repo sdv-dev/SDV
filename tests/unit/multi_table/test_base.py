db1c0745b6753edef40cc3ca5f60249f605e5a2c
Plamen Valentinov Kolev <41479552+pvk-developer@users.noreply.github.com>
2022-11-09 21:22:19 +0100

    Add validate to `BaseMultiTableSynthesizer` (#1092)
    * Add fk validation
    
    * Curate message
    
    * Finish unit tests
    
    * Address comments
    
    * Fix error messaging
    
    * Address comments

--- tests/unit/multi_table/test_base.py
+++ tests/unit/multi_table/test_base.py
@@ -1,8 +1,14 @@
+import re
 from collections import defaultdict
 from unittest.mock import Mock, call
 
+import numpy as np
+import pandas as pd
+import pytest
+
 from sdv.multi_table.base import BaseMultiTableSynthesizer
 from sdv.single_table.copulas import GaussianCopulaSynthesizer
+from sdv.single_table.errors import InvalidDataError
 from tests.utils import get_multi_table_metadata
 
 
@@ -136,3 +142,203 @@ class TestBaseMultiTableSynthesizer:
 
         # Assert
         assert metadata == result
+
+    def test__validate_foreign_keys(self):
+        """Test that when the data matches as expected there are no errors."""
+        # Setup
+        metadata = get_multi_table_metadata()
+        data = {
+            'nesreca': pd.DataFrame({
+                'id_nesreca': np.arange(10),
+                'upravna_enota': np.arange(10),
+            }),
+            'oseba': pd.DataFrame({
+                'upravna_enota': np.arange(10),
+                'id_nesreca': np.arange(10),
+            }),
+            'upravna_enota': pd.DataFrame({
+                'id_upravna_enota': np.arange(10),
+            }),
+        }
+        instance = BaseMultiTableSynthesizer(metadata)
+
+        # Run
+        result = instance._validate_foreign_keys(data)
+
+        # Assert
+        assert result is None
+
+    def test__validate_foreign_keys_missing_keys(self):
+        """Test that errors are being returned.
+
+        When the values of the foreign keys are not within the values of the parent
+        primary key, a list of errors must be returned indicating the values that are missing.
+        """
+        # Setup
+        metadata = get_multi_table_metadata()
+        data = {
+            'nesreca': pd.DataFrame({
+                'id_nesreca': np.arange(0, 20, 2),
+                'upravna_enota': np.arange(10),
+            }),
+            'oseba': pd.DataFrame({
+                'upravna_enota': np.arange(10, 20),
+                'id_nesreca': np.arange(10),
+            }),
+            'upravna_enota': pd.DataFrame({
+                'id_upravna_enota': np.arange(10),
+            }),
+        }
+        instance = BaseMultiTableSynthesizer(metadata)
+
+        # Run
+        result = instance._validate_foreign_keys(data)
+
+        # Assert
+        missing_upravna_enota = (
+            'Relationships:\n'
+            "Error: foreign key column 'upravna_enota' contains unknown references: "
+            '(10, 11, 12, 13, 14, + more). '
+            'All the values in this column must reference a primary key.\n'
+            "Error: foreign key column 'id_nesreca' contains unknown references: (1, 3, 5, 7, 9)."
+            ' All the values in this column must reference a primary key.'
+        )
+        assert result == missing_upravna_enota
+
+    def test_validate(self):
+        """Test that no error is being raised when the data is valid."""
+        # Setup
+        metadata = get_multi_table_metadata()
+        data = {
+            'nesreca': pd.DataFrame({
+                'id_nesreca': np.arange(10),
+                'upravna_enota': np.arange(10),
+            }),
+            'oseba': pd.DataFrame({
+                'upravna_enota': np.arange(10),
+                'id_nesreca': np.arange(10),
+            }),
+            'upravna_enota': pd.DataFrame({
+                'id_upravna_enota': np.arange(10),
+            }),
+        }
+
+        instance = BaseMultiTableSynthesizer(metadata)
+
+        # Run and Assert
+        instance.validate(data)
+
+    def test_validate_missing_table(self):
+        """Test that an error is being raised when there is a missing table in the dictionary."""
+        # Setup
+        metadata = get_multi_table_metadata()
+        data = {
+            'nesrecas': pd.DataFrame({
+                'id_nesreca': np.arange(10),
+                'upravna_enota': np.arange(10),
+            }),
+            'oseba': pd.DataFrame({
+                'upravna_enota': np.arange(10),
+                'id_nesreca': np.arange(10),
+            }),
+            'upravna_enota': pd.DataFrame({
+                'id_upravna_enota': np.arange(10),
+            }),
+        }
+
+        instance = BaseMultiTableSynthesizer(metadata)
+
+        # Run and Assert
+        error_msg = "The provided data is missing the tables {'nesreca'}."
+        with pytest.raises(InvalidDataError, match=error_msg):
+            instance.validate(data)
+
+    def test_validate_data_is_not_dataframe(self):
+        """Test that an error is being raised when the data is not a dataframe."""
+        # Setup
+        metadata = get_multi_table_metadata()
+        data = {
+            'nesreca': pd.Series({
+                'id_nesreca': np.arange(10),
+                'upravna_enota': np.arange(10),
+            }),
+            'oseba': pd.DataFrame({
+                'upravna_enota': np.arange(10),
+                'id_nesreca': np.arange(10),
+            }),
+            'upravna_enota': pd.DataFrame({
+                'id_upravna_enota': np.arange(10),
+            }),
+        }
+
+        instance = BaseMultiTableSynthesizer(metadata)
+
+        # Run and Assert
+        error_msg = "Data must be a DataFrame, not a <class 'pandas.core.series.Series'>."
+        with pytest.raises(InvalidDataError, match=error_msg):
+            instance.validate(data)
+
+    def test_validate_data_does_not_match(self):
+        """Test that an error is being raised when the data does not match the metadata."""
+        # Setup
+        metadata = get_multi_table_metadata()
+        data = {
+            'nesreca': pd.DataFrame({
+                'id_nesreca': np.arange(10).astype(str),
+                'upravna_enota': np.arange(10),
+            }),
+            'oseba': pd.DataFrame({
+                'upravna_enota': np.arange(10).astype(str),
+                'id_nesreca': np.arange(10),
+            }),
+            'upravna_enota': pd.DataFrame({
+                'id_upravna_enota': np.arange(10).astype(str),
+            }),
+        }
+
+        instance = BaseMultiTableSynthesizer(metadata)
+
+        # Run and Assert
+        error_msg = re.escape(
+            'The provided data does not match the metadata:\n'
+            "Table: 'nesreca'\n"
+            "Error: Invalid values found for numerical column 'id_nesreca': ['0', '1', '2', "
+            "'+ 7 more']."
+            "\n\nTable: 'oseba'\n"
+            "Error: Invalid values found for numerical column 'upravna_enota': ['0', '1', '2', "
+            "'+ 7 more']."
+            "\n\nTable: 'upravna_enota'\n"
+            "Error: Invalid values found for numerical column 'id_upravna_enota': ['0', '1', '2', "
+            "'+ 7 more']."
+        )
+        with pytest.raises(InvalidDataError, match=error_msg):
+            instance.validate(data)
+
+    def test_validate_missing_foreign_keys(self):
+        """Test that errors are being raised when there are missing foreign keys."""
+        # Setup
+        metadata = get_multi_table_metadata()
+        data = {
+            'nesreca': pd.DataFrame({
+                'id_nesreca': np.arange(0, 20, 2),
+                'upravna_enota': np.arange(10),
+            }),
+            'oseba': pd.DataFrame({
+                'upravna_enota': np.arange(10),
+                'id_nesreca': np.arange(10),
+            }),
+            'upravna_enota': pd.DataFrame({
+                'id_upravna_enota': np.arange(10),
+            }),
+        }
+        instance = BaseMultiTableSynthesizer(metadata)
+
+        # Run and Assert
+        error_msg = re.escape(
+            'The provided data does not match the metadata:\n'
+            'Relationships:\n'
+            "Error: foreign key column 'id_nesreca' contains unknown references: (1, 3, 5, 7, 9). "
+            'All the values in this column must reference a primary key.'
+        )
+        with pytest.raises(InvalidDataError, match=error_msg):
+            instance.validate(data)
