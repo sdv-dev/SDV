ce89d0f44976c3da97ca0f2e8161fdf4427f1f0f
Plamen Valentinov Kolev <pvkdeveloper@gmx.com>
2022-11-17 01:36:50 +0100

    Progress on tests

--- tests/unit/multi_table/test_hma.py
+++ tests/unit/multi_table/test_hma.py
@@ -1,5 +1,8 @@
 from unittest.mock import Mock
 
+import numpy as np
+import pandas as pd
+
 from sdv.multi_table.hma import HMASynthesizer
 from sdv.single_table.copulas import GaussianCopulaSynthesizer
 from tests.utils import get_multi_table_metadata
@@ -25,3 +28,55 @@ class TestHMASynthesizer:
             'upravna_enota': {'default_distribution': 'beta'},
         }
         instance.metadata.validate.assert_called_once_with()
+
+    def test__get_extension(self):
+        """Test the ``_get_extension`` method.
+
+        Test that the resulting dataframe contains extended columns using the names
+        and parameters from a trained ``copulas.univariate`` model.
+        """
+        # Setup
+        metadata = get_multi_table_metadata()
+        child_table = pd.DataFrame({
+            'id_nesreca': [0, 1, 2, 3],
+            'upravna_enota': [0, 1, 2, 3]
+        })
+        instance = HMASynthesizer(metadata)
+
+        # Run
+        result = instance._get_extension('nesreca', child_table, 'upravna_enota')
+
+        # Assert
+        expected = pd.DataFrame({
+            '__nesreca__upravna_enota__univariates__id_nesreca__a': [1., 1., 1., 1.],
+            '__nesreca__upravna_enota__univariates__id_nesreca__b': [1., 1., 1., 1.],
+            '__nesreca__upravna_enota__univariates__id_nesreca__loc': [0., 1., 2., 3.],
+            '__nesreca__upravna_enota__univariates__id_nesreca__scale': [np.nan] * 4,
+            '__nesreca__upravna_enota__num_rows': [1., 1., 1., 1.]
+        })
+
+        pd.testing.assert_frame_equal(result, expected)
+
+    def test__get_foreign_keys(self):
+        """Test that this method returns the foreign keys for a given table name and child name."""
+        # Setup
+        metadata = get_multi_table_metadata()
+        instance = HMASynthesizer(metadata)
+
+        # Run
+        result = instance._get_foreign_keys('nesreca', 'oseba')
+
+        # Assert
+        assert result == ['id_nesreca']
+
+    def test__get_all_foreign_keys(self):
+        """Test that this method returns all the foreign keys for a given table name."""
+        # Setup
+        metadata = get_multi_table_metadata()
+        instance = HMASynthesizer(metadata)
+
+        # Run
+        result = instance._get_all_foreign_keys('nesreca')
+
+        # Assert
+        assert result == ['upravna_enota']
