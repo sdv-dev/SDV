29155d0813689d175dc3f7efed2cd7d1b1c1720f
Katharine Xiao <2405771+katxiao@users.noreply.github.com>
2022-12-06 09:29:19 -0500

    Update sdmetrics version (#1091)

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ install_requires = [
     'ctgan>=0.5.2,<0.6',
     'deepecho>=0.3.0.post1,<0.4',
     'rdt>=1.2.0,<1.3.0',
-    'sdmetrics>=0.7.0.dev0,<0.8',
+    'sdmetrics>=0.8.0,<0.9',
     'cloudpickle>=2.1.0,<3.0'
 ]
 
