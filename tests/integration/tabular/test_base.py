a2063473054ff7564221b40226a80eaa96f3cef3
Plamen Valentinov Kolev <41479552+pvk-developer@users.noreply.github.com>
2022-12-16 21:16:59 +0100

    Add `Sampling` to `BaseMultiTableSynthesizer` and `HMASynthesizer` (#1114)
    * Base sample and hma sample
    
    * Fix sampling num rows
    
    * Undo elif
    
    * Add set_parameters test
    
    * Add tests
    
    * Fix np 64 bits
    
    * Address comments
    
    * Fix lint
    
    * Fix get distribution and add explainatory comments
    
    * Address comments
    
    * Fix test
    
    * Fix unit test
    
    * Remove .value inclusion
    
    * Fix alternate keys being foreign keys
    
    * Adapt to new RDT and fix tests.
    
    * Remove tutorial builds for now.
    
    * Address comments

--- tests/integration/tabular/test_base.py
+++ tests/integration/tabular/test_base.py
@@ -285,12 +285,12 @@ def test_conditional_sampling_constraint_uses_reject_sampling(gm_mock, isinstanc
     model = GaussianCopula(constraints=[constraint], categorical_transformer='LabelEncoder')
     sampled_numeric_data = [
         pd.DataFrame({
-            'city#state.value': [0, 1, 2, 0, 0],
-            'age.value': [30, 30, 30, 30, 30]
+            'city#state': [0, 1, 2, 0, 0],
+            'age': [30, 30, 30, 30, 30]
         }),
         pd.DataFrame({
-            'city#state.value': [1],
-            'age.value': [30]
+            'city#state': [1],
+            'age': [30]
         })
     ]
     gm_mock.return_value.sample.side_effect = sampled_numeric_data
