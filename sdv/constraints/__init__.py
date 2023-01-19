2722a6e222db6f1f5efdb87559d7345ed3f68132
Plamen Valentinov Kolev <pvkdeveloper@gmx.com>
2023-01-19 16:59:17 +0100

    Rename 'create_custom_constraint' to 'create_custom_constraint_class'

--- sdv/constraints/__init__.py
+++ sdv/constraints/__init__.py
@@ -3,10 +3,10 @@
 from sdv.constraints.base import Constraint
 from sdv.constraints.tabular import (
     FixedCombinations, FixedIncrements, Inequality, Negative, OneHotEncoding, Positive, Range,
-    ScalarInequality, ScalarRange, Unique, create_custom_constraint)
+    ScalarInequality, ScalarRange, Unique, create_custom_constraint_class)
 
 __all__ = [
-    'create_custom_constraint',
+    'create_custom_constraint_class',
     'Constraint',
     'Inequality',
     'ScalarInequality',
