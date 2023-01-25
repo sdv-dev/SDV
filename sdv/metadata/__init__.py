93f26bcbb365a85510048d5b971c962ef975d91e
Plamen Valentinov Kolev <pvkdeveloper@gmx.com>
2023-01-26 00:13:55 +0100

    Remove the rest of the code and fix some tests

--- sdv/metadata/__init__.py
+++ sdv/metadata/__init__.py
@@ -1,18 +1,14 @@
 """Metadata module."""
 
 from sdv.metadata import visualization
-from sdv.metadata.dataset import Metadata
 from sdv.metadata.errors import InvalidMetadataError, MetadataNotFittedError
 from sdv.metadata.multi_table import MultiTableMetadata
 from sdv.metadata.single_table import SingleTableMetadata
-from sdv.metadata.table import Table
 
 __all__ = (
-    'Metadata',
     'InvalidMetadataError',
     'MetadataNotFittedError',
     'MultiTableMetadata',
     'SingleTableMetadata',
-    'Table',
     'visualization'
 )
