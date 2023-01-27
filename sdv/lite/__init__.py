c1b1582bcb7c7a43d9ffb173fc9f52d51fc94bb3
Plamen Valentinov Kolev <41479552+pvk-developer@users.noreply.github.com>
2023-01-27 18:40:54 +0100

    Remove old code (#1183)
    * Remove lite legacy code
    
    * Remove relational legacy code
    
    * Remove tabular legacy code
    
    * Remove timeseries legacy code
    
    * Remove legacy code from metadata
    
    * Remove old tutorials
    
    * Remove timeseries and tabular integration tests
    
    * Remove relational integration tests
    
    * Remove lite tabular preset
    
    * Remove unit tests for metadata, tabular and lite
    
    * Remove the rest of the code and fix some tests
    
    * Fix test
    
    * Fix test
    
    * remove old tests
    
    * Remove tabular tests
    
    * Delete readme runs

--- sdv/lite/__init__.py
+++ sdv/lite/__init__.py
@@ -1,9 +1,7 @@
 """SDV lite package that contains model presets."""
 
 from sdv.lite.single_table import SingleTablePreset
-from sdv.lite.tabular import TabularPreset
 
 __all__ = (
     'SingleTablePreset',
-    'TabularPreset',
 )
