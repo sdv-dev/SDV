bf9a4aad42ede9d01f346c655e6440ece597047f
Plamen Valentinov Kolev <pvkdeveloper@gmx.com>
2023-02-07 18:50:04 +0100

    Fix order in which transformers are returned

--- sdv/single_table/base.py
+++ sdv/single_table/base.py
@@ -352,6 +352,11 @@ class BaseSynthesizer:
                 "Use 'auto_assign_transformers' or 'fit' to create them."
             )
 
+        field_transformers = {
+            column_name: field_transformers.get(column_name)
+            for column_name in self.metadata._columns
+        }
+
         return field_transformers
 
     def get_info(self):
