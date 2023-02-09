bb7b4af7dfdce0550f830df696ca183ed729f926
Plamen Valentinov Kolev <41479552+pvk-developer@users.noreply.github.com>
2023-02-09 17:48:21 +0100

    Improve PII handling and add tests (#1244)
    * Improve PII handling and add tests
    
    * Address comments
    
    * Rename valuerror to invalidmetadata

--- tests/unit/metadata/test_single_table.py
+++ tests/unit/metadata/test_single_table.py
@@ -481,6 +481,52 @@ class TestSingleTableMetadata:
             'phrase', 'text', regex_format='[A-z0-9]', pii=True)
         mock__validate_text.assert_called_once_with('phrase', regex_format='[A-z0-9]', pii=True)
 
+    @patch('sdv.metadata.single_table.SingleTableMetadata._validate_unexpected_kwargs')
+    def test__validate_pii_not_true_or_false(self, mock__validate_kwargs):
+        """Test ``_validate_column`` method when ``pii`` is not ``True``or ``False``."""
+        # Run and Assert
+        error_msg = re.escape(
+            "Parameter 'pii' is set to an invalid attribute ('some_text') for column 'address'. "
+            'Expected a value of True or False.'
+        )
+        with pytest.raises(InvalidMetadataError, match=error_msg):
+            SingleTableMetadata._validate_pii('address', pii='some_text')
+
+    @patch('sdv.metadata.single_table.SingleTableMetadata._validate_unexpected_kwargs')
+    def test__validate_pii(self, mock__validate_kwargs):
+        """Test ``_validate_column`` method when ``pii`` is ``True``or ``False``."""
+        # Run and Assert
+        SingleTableMetadata._validate_pii('address', pii=True)
+
+    def test_update_column_sdtype(self):
+        """Test that ``update_column`` updates the sdtype and keyword args for the given column."""
+        # Setup
+        instance = SingleTableMetadata()
+        instance._columns = {'a': {'sdtype': 'numerical'}}
+
+        # Run
+        instance.update_column('a', sdtype='categorical', order_by='alphabetical')
+
+        # Assert
+        assert instance._columns == {'a': {'sdtype': 'categorical', 'order_by': 'alphabetical'}}
+
+    def test_update_column_add_extra_value(self):
+        """Test that ``update_column`` updates only the keyword args for the given column."""
+        # Setup
+        instance = SingleTableMetadata()
+        instance._columns = {'a': {'sdtype': 'numerical'}}
+
+        # Run
+        instance.update_column('a', computer_representation='Int64')
+
+        # Assert
+        assert instance._columns == {
+            'a': {
+                'sdtype': 'numerical',
+                'computer_representation': 'Int64'
+            }
+        }
+
     def test_add_column_column_name_in_columns(self):
         """Test ``add_column`` method.
 
