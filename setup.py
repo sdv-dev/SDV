1c34b3221b9e321d7e84b14ba67a8eb292684983
Frances Hartwell <frances@datacebo.com>
2023-03-24 13:44:24 -0400

    Update README.md for v1.0 (#1333)
    * V1.0 README updates
    
    * update another docs link
    
    * better dark mode logos + fix a link
    
    * fix typo
    
    * change dev status to 5 - Production/Stable
    
    * update image links to point to master branch

--- setup.py
+++ setup.py
@@ -106,7 +106,7 @@ setup(
     author='DataCebo, Inc.',
     author_email='info@sdv.dev',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: Free for non-commercial use',
         'Natural Language :: English',
