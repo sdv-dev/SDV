7ed90a86d032255841eeea3efc0d8bf884adddc6
Frances Hartwell <frances.hartwell@alteryx.com>
2023-03-29 11:42:03 -0400

    initial version addon

--- sdv/__init__.py
+++ sdv/__init__.py
@@ -7,3 +7,22 @@
 __author__ = 'DataCebo, Inc.'
 __email__ = 'info@sdv.dev'
 __version__ = '1.0.1.dev0'
+
+import warnings
+from importlib.metadata import entry_points
+
+
+def _add_version():
+    for entry_point in entry_points(group='sdv_modules', name='version'):
+        try:
+            module = entry_point.load()
+        except Exception:
+            msg = f'Failed to load "{entry_point.name}" from "{entry_point.module}". '
+            warnings.warn(msg)
+            continue
+
+        if 'version' not in globals():
+            globals()['version'] = module
+
+
+_add_version()
