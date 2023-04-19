3a3384c89efa6b55b47a64dcf0d8025fc13c2624
Andrew Montanez <andrew@sdv.dev>
2023-04-18 20:49:07 -0500

    trying to fix test again

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -9,14 +9,14 @@ from tests.utils import SeriesMatcher
 
 @patch('sdv.utils.pd.to_timedelta')
 def test_convert_to_timedelta(to_timedelta_mock):
-    """Test that """
+    """Test that nans and values are properly converted to timedeltas."""
     # Setup
     column = pd.Series([7200, 3600, np.nan])
     to_timedelta_mock.return_value = pd.Series([
         pd.Timedelta(hours=1),
         pd.Timedelta(hours=2),
         pd.Timedelta(hours=0)
-    ])
+    ], dtype='timedelta64[ns]')
 
     # Run
     converted_column = convert_to_timedelta(column)
@@ -27,5 +27,5 @@ def test_convert_to_timedelta(to_timedelta_mock):
         pd.Timedelta(hours=1),
         pd.Timedelta(hours=2),
         pd.NaT
-    ])
+    ], dtype='timedelta64[ns]')
     pd.testing.assert_series_equal(converted_column, expected_column)
