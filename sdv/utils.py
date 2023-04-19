41690bd97a50e0ba17921f6293d4385da17d96a8
R-Palazzo <romainpalazzo@gmail.com>
2023-04-19 16:02:14 +0100

    add unit test

--- sdv/utils.py
+++ sdv/utils.py
@@ -167,5 +167,5 @@ def load_data_from_csv(filepath, pandas_kwargs=None):
 
 
 def groupby_list(list_to_check):
-    """Return the list element if the length is 1 and the list otherwise."""
+    """Return the first element of the list if the length is 1 else the entire list."""
     return list_to_check[0] if len(list_to_check) == 1 else list_to_check
