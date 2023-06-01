a55b0e34cc3c692331a75361a530fd42182fae4d
Frances Hartwell <frances.hartwell@alteryx.com>
2023-06-01 14:20:12 -0400

    Detect all addons from top level init

--- sdv/multi_table/__init__.py
+++ sdv/multi_table/__init__.py
@@ -1,10 +1,7 @@
 """Synthesizers for Multi Table data."""
 
-from sdv._addons import _find_addons
 from sdv.multi_table.hma import HMASynthesizer
 
 __all__ = (
     'HMASynthesizer',
 )
-
-_find_addons('sdv.multi_table_modules', globals(), add_all=True)
