50392fad48e6df3553254b5dc72fd0dae2ece30e
Frances Hartwell <frances@datacebo.com>
2023-06-21 14:54:43 -0400

    Use HierarchicalSampler mixin with HMA (#1449)
    * switch HMA over to using HierarchicalSampler mixin
    
    * comments

--- sdv/sampling/__init__.py
+++ sdv/sampling/__init__.py
@@ -1,7 +1,11 @@
 """SDV Sampling module."""
 
+from sdv.sampling.hierarchical_sampler import BaseHierarchicalSampler
+from sdv.sampling.independent_sampler import BaseIndependentSampler
 from sdv.sampling.tabular import Condition
 
 __all__ = [
+    'BaseHierarchicalSampler',
+    'BaseIndependentSampler',
     'Condition',
 ]
