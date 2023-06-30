56eecccc64840cf117889ef2439dfa3f113edc35
Plamen Valentinov Kolev <pvkdeveloper@gmx.com>
2023-06-30 18:36:15 +0200

    Undo changes for 3.7

--- sdv/constraints/tabular.py
+++ sdv/constraints/tabular.py
@@ -712,10 +712,7 @@ class ScalarInequality(Constraint):
             diff_column = diff_column.round()
 
         if self._is_datetime:
-            nan_mask = pd.isna(diff_column)
-            diff_column[nan_mask] = 0
             diff_column = diff_column.astype('timedelta64[ns]')
-            diff_column[nan_mask] = pd.NaT
 
         if self._operator in [np.greater, np.greater_equal]:
             original_column = self._value + diff_column
