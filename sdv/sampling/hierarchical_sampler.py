5ea2d1fa84932fff0d4031bcfb131ef6c8068505
Felipe <fealho@gmail.com>
2023-09-18 18:35:02 -0700

    Fix lint

--- sdv/sampling/hierarchical_sampler.py
+++ sdv/sampling/hierarchical_sampler.py
@@ -217,5 +217,4 @@ class BaseHierarchicalSampler():
                 )
                 added_relationships.add((parent_name, child_name))
 
-        print(sampled_data['parent'].columns)
         return self._finalize(sampled_data)
