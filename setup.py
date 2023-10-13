808fc947bb0722651d27b48e8c6cbe0d01f69c76
Andrew Montanez <andrew@sdv.dev>
2023-10-13 12:28:40 -0500

    Bump version: 1.5.0.dev1 → 1.5.0

--- setup.py
+++ setup.py
@@ -137,6 +137,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/sdv-dev/SDV',
-    version='1.5.0.dev1',
+    version='1.5.0',
     zip_safe=False,
 )
