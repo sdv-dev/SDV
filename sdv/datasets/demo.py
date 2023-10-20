fac8b25c2462633933370ec2a96f8e7a5d9d9f17
lajohn4747 <john@datacebo.com>
2023-10-20 15:19:49 -0500

    Use to_numeric and accept float dtype if Nan

--- sdv/datasets/demo.py
+++ sdv/datasets/demo.py
@@ -171,9 +171,8 @@ def get_available_demos(modality):
             headers = client.head_object(Bucket=BUCKET, Key=item['Key'])['Metadata']
             size_mb = headers.get('size-mb', np.nan)
             tables_info['size_MB'].append(round(float(size_mb), 2))
-            num_tables = headers.get('num-tables', pd.NA)
-            if not pd.isna(num_tables):
-                num_tables = int(num_tables)
-            tables_info['num_tables'].append(num_tables)
+            tables_info['num_tables'].append(headers.get('num-tables', np.nan))
 
-    return pd.DataFrame(tables_info)
+    df = pd.DataFrame(tables_info)
+    df['num_tables'] = pd.to_numeric(df['num_tables'])
+    return df
