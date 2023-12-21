cc1406fb18a99313420363df4fa2f419c9e9e553
R-Palazzo <romainpalazzo@gmail.com>
2023-12-21 15:08:39 +0100

    update unit tests for column relationship validation

--- tests/unit/metadata/test_validation.py
+++ tests/unit/metadata/test_validation.py
@@ -1,26 +1,48 @@
 """Test Single Table Metadata."""
-
 import re
+from unittest.mock import patch
 
 import pytest
+from rdt.errors import TransformerInputError
 
 from sdv.metadata.errors import InvalidMetadataError
 from sdv.metadata.validation import validate_address_sdtypes
 
 
-def test_validate_address_sdtypes():
+@patch('sdv.metadata.validation._check_import_address_transformers')
+@patch('rdt.transformers')
+def test_validate_address_sdtypes(mock_transformers, mock_check_import):
     """Test address sdtype validation."""
     # Setup
-    column_metadata = {
+    columns_to_sdtypes = {
         'col_1': {'sdtype': 'id'},
         'col_2': {'sdtype': 'numerical'},
         'col_3': {'sdtype': 'state'}
     }
+    mock_validate_sdtypes = mock_transformers.address.RandomLocationGenerator._validate_sdtypes
+
+    # Run
+    validate_address_sdtypes(columns_to_sdtypes)
+
+    # Asserts
+    mock_check_import.assert_called_once()
+    mock_validate_sdtypes.assert_called_once_with(columns_to_sdtypes)
+
+
+@patch('sdv.metadata.validation._check_import_address_transformers')
+@patch('rdt.transformers')
+def test_validate_address_sdtypes_error(mock_transformers, mock_check_import):
+    """Test address sdtype validation."""
+    # Setup
+    columns_to_sdtypes = {
+        'col_1': {'sdtype': 'id'},
+        'col_2': {'sdtype': 'numerical'},
+        'col_3': {'sdtype': 'state'}
+    }
+    mock_validate_sdtypes = mock_transformers.address.RandomLocationGenerator._validate_sdtypes
+    mock_validate_sdtypes.side_effect = TransformerInputError('Error')
 
     # Run and Assert
-    err_msg = re.escape(
-        "Columns ['col_1', 'col_2'] have unsupported sdtypes for column "
-        "relationship type 'address'"
-    )
-    with pytest.raises(InvalidMetadataError, match=err_msg):
-        validate_address_sdtypes(column_metadata, ['col_1', 'col_2', 'col_3', 'col_4'])
+    expected_message = re.escape('Error')
+    with pytest.raises(InvalidMetadataError, match=expected_message):
+        validate_address_sdtypes(columns_to_sdtypes)
