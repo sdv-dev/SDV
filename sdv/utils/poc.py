eebac5d878a5a9f601a87f2e1e0cfca42b146918
R-Palazzo <romainpalazzo@gmail.com>
2024-03-26 18:56:11 +0000

    add _print_simplified_schema_summary

--- sdv/utils/poc.py
+++ sdv/utils/poc.py
@@ -7,8 +7,8 @@ from sdv._utils import _validate_foreign_keys_not_null
 from sdv.errors import InvalidDataError, SynthesizerInputError
 from sdv.multi_table.hma import MAX_NUMBER_OF_COLUMNS
 from sdv.multi_table.utils import (
-    _get_relationship_for_child, _get_rows_to_drop, _get_total_estimated_columns, _simplify_data,
-    _simplify_metadata)
+    _get_relationship_for_child, _get_rows_to_drop, _get_total_estimated_columns,
+    _print_simplified_schema_summary, _simplify_data, _simplify_metadata)
 
 
 def drop_unknown_references(metadata, data, drop_missing_values=True, verbose=True):
@@ -100,9 +100,11 @@ def simplify_schema(data, metadata):
     """
     total_estimated_columns = _get_total_estimated_columns(metadata)
     if total_estimated_columns <= MAX_NUMBER_OF_COLUMNS:
+        _print_simplified_schema_summary(data, data)
         return data, metadata
 
     simple_metadata = _simplify_metadata(metadata)
     simple_data = _simplify_data(data, simple_metadata)
+    _print_simplified_schema_summary(data, simple_data)
 
     return simple_data, simple_metadata
