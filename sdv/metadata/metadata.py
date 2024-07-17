089f376c9710a7c030c3115e2a80fa05882246d3
John La <lajohn4747@users.noreply.github.com>
2024-07-17 11:47:50 -0500

    Add Metadata Class (#2135)

---
+++ sdv/metadata/metadata.py
@@ -0,0 +1,73 @@
+"""Metadata."""
+
+from pathlib import Path
+
+from sdv.metadata.multi_table import MultiTableMetadata
+from sdv.metadata.single_table import SingleTableMetadata
+from sdv.metadata.utils import read_json
+
+
+class Metadata(MultiTableMetadata):
+    """Metadata class that handles all metadata."""
+
+    METADATA_SPEC_VERSION = 'V1'
+
+    @classmethod
+    def load_from_json(cls, filepath):
+        """Create a ``Metadata`` instance from a ``json`` file.
+
+        Args:
+            filepath (str):
+                String that represents the ``path`` to the ``json`` file.
+
+        Raises:
+            - An ``Error`` if the path does not exist.
+            - An ``Error`` if the ``json`` file does not contain the ``METADATA_SPEC_VERSION``.
+
+        Returns:
+            A ``Metadata`` instance.
+        """
+        filename = Path(filepath).stem
+        metadata = read_json(filepath)
+        return cls.load_from_dict(metadata, filename)
+
+    @classmethod
+    def load_from_dict(cls, metadata_dict, single_table_name=None):
+        """Create a ``Metadata`` instance from a python ``dict``.
+
+        Args:
+            metadata_dict (dict):
+                Python dictionary representing a ``MultiTableMetadata``
+                or ``SingleTableMetadata`` object.
+            single_table_name (string):
+                If the python dictionary represents a ``SingleTableMetadata`` then
+                this arg is used for the name of the table.
+
+        Returns:
+            Instance of ``Metadata``.
+        """
+        instance = cls()
+        instance._set_metadata_dict(metadata_dict, single_table_name)
+        return instance
+
+    def _set_metadata_dict(self, metadata, single_table_name=None):
+        """Set a ``metadata`` dictionary to the current instance.
+
+        Checks to see if the metadata is in the ``SingleTableMetadata`` or
+        ``MultiTableMetadata`` format and converts it to a standard
+        ``MultiTableMetadata`` format if necessary.
+
+        Args:
+            metadata (dict):
+                Python dictionary representing a ``MultiTableMetadata`` or
+                ``SingleTableMetadata`` object.
+        """
+        is_multi_table = 'tables' in metadata
+
+        if is_multi_table:
+            super()._set_metadata_dict(metadata)
+        else:
+            if single_table_name is None:
+                single_table_name = 'default_table_name'
+
+            self.tables[single_table_name] = SingleTableMetadata.load_from_dict(metadata)
