cd3f1a33378310b34949f9b518c9b5875d28f232
John La <lajohn4747@users.noreply.github.com>
2024-07-17 11:47:50 -0500

    Add Metadata Class (#2135)

--- sdv/metadata/__init__.py
+++ sdv/metadata/__init__.py
@@ -4,9 +4,11 @@ from sdv.metadata import visualization
 from sdv.metadata.errors import InvalidMetadataError, MetadataNotFittedError
 from sdv.metadata.multi_table import MultiTableMetadata
 from sdv.metadata.single_table import SingleTableMetadata
+from sdv.metadata.metadata import Metadata
 
 __all__ = (
     'InvalidMetadataError',
+    'Metadata',
     'MetadataNotFittedError',
     'MultiTableMetadata',
     'SingleTableMetadata',
