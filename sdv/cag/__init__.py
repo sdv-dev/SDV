5331021de07775356e141a3d1228d8198fe2e6af
Felipe <fealho@gmail.com>
2025-03-10 19:54:08 -0700

    Add OneHotEncoding CAG

--- sdv/cag/__init__.py
+++ sdv/cag/__init__.py
@@ -4,5 +4,12 @@ from sdv.cag.fixed_combinations import FixedCombinations
 from sdv.cag.fixed_increments import FixedIncrements
 from sdv.cag.inequality import Inequality
 from sdv.cag.range import Range
+from sdv.cag.one_hot_encoding import OneHotEncoding
 
-__all__ = ('FixedCombinations', 'FixedIncrements', 'Inequality', 'Range')
+__all__ = (
+    'FixedCombinations',
+    'FixedIncrements',
+    'Inequality',
+    'Range',
+    'OneHotEncoding',
+)
