96ee865627529878d7715568fbb728fcf3217092
Felipe Alex Hofmann <fealho@gmail.com>
2025-03-12 06:24:38 -0700

    Add Inequality CAG (#2405)
    Co-authored-by: Gaurav Sheni <gvsheni@gmail.com>

--- sdv/cag/__init__.py
+++ sdv/cag/__init__.py
@@ -2,5 +2,6 @@
 
 from sdv.cag.fixed_combinations import FixedCombinations
 from sdv.cag.fixed_increments import FixedIncrements
+from sdv.cag.inequality import Inequality
 
-__all__ = ('FixedCombinations', 'FixedIncrements')
+__all__ = ('FixedCombinations', 'FixedIncrements', 'Inequality')
