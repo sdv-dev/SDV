bec442789d2b9d7073fbd44c273a304580e16bd7
Felipe Alex Hofmann <fealho@gmail.com>
2025-03-12 07:30:52 -0700

    Add Range CAG (#2407)
    Co-authored-by: Gaurav Sheni <gvsheni@gmail.com>

--- sdv/cag/__init__.py
+++ sdv/cag/__init__.py
@@ -3,5 +3,6 @@
 from sdv.cag.fixed_combinations import FixedCombinations
 from sdv.cag.fixed_increments import FixedIncrements
 from sdv.cag.inequality import Inequality
+from sdv.cag.range import Range
 
-__all__ = ('FixedCombinations', 'FixedIncrements', 'Inequality')
+__all__ = ('FixedCombinations', 'FixedIncrements', 'Inequality', 'Range')
