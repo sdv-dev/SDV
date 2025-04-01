d452fd7848d1284f492b1eb477d246cef3907999
R-Palazzo <116157184+R-Palazzo@users.noreply.github.com>
2025-04-01 12:57:35 +0100

    Provide a more descriptive error message when Regex is is not supported (#2441)

--- sdv/_utils.py
+++ sdv/_utils.py
@@ -12,7 +12,7 @@ from pathlib import Path
 import pandas as pd
 from pandas.api.types import is_float, is_integer
 from pandas.core.tools.datetimes import _guess_datetime_format_for_array
-from rdt.transformers.utils import _GENERATORS
+from rdt.transformers.utils import _GENERATORS, strings_from_regex
 
 from sdv import version
 from sdv.errors import SDVVersionWarning, SynthesizerInputError, VersionError
@@ -461,3 +461,16 @@ def _get_transformer_init_kwargs(transformer):
         for key in args
         if key != 'model_missing_values' and hasattr(transformer, key)
     }
+
+
+def _check_regex_format(table_name, column_name, regex):
+    """Check if SDV can generate data for the given regex."""
+    if regex:
+        try:
+            strings_from_regex(regex)
+        except Exception as e:
+            raise SynthesizerInputError(
+                "SDV synthesizers do not currently support complex regex formats such as '"
+                f"{regex}', which you have provided for table '{table_name}', column '{column_name}"
+                "'. Please use a simplified format or update to a different sdtype."
+            ) from e
