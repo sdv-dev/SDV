98b9c3b366f9bde4b838fa18de753ad2c990f3ee
rwedge <roy.wedge@datacebo.com>
2025-04-11 12:32:46 -0500

    remove small numeric dtypes; update float32 and period dtypes

--- tests/benchmark/pandas_dtypes.py
+++ tests/benchmark/pandas_dtypes.py
@@ -41,16 +41,12 @@ PANDAS_DTYPES = {
 }
 
 PANDAS_DATETIME_DTYPES = {
-    'pd.Int8 - datetime': pd.DataFrame({'pd.Int8 - datetime': pd.Series([1, 2, -3, None, 4, 5], dtype='Int8')}),
-    'pd.Int16 - datetime': pd.DataFrame({'pd.Int16 - datetime': pd.Series([1, 2, -3, None, 4, 5], dtype='Int16')}),
     'pd.Int32 - datetime': pd.DataFrame({'pd.Int32 - datetime': pd.Series([20240101, 20240630, 20250101, None, 20250601, 20260101], dtype='Int32')}),
     'pd.Int64 - datetime': pd.DataFrame({'pd.Int64 - datetime': pd.Series([20240101, 20240630, 20250101, None, 20250601, 20260101], dtype='Int64')}),
-    'pd.UInt8 - datetime': pd.DataFrame({'pd.UInt8 - datetime': pd.Series([1, 2, 3, None, 4, 5], dtype='UInt8')}),
-    'pd.UInt16 - datetime': pd.DataFrame({'pd.UInt16 - datetime': pd.Series([1, 2, 3, None, 4, 5], dtype='UInt16')}),
     'pd.UInt32 - datetime': pd.DataFrame({'pd.UInt32 - datetime': pd.Series([20240101, 20240630, 20250101, None, 20250601, 20260101], dtype='UInt32')}),
     'pd.UInt64 - datetime': pd.DataFrame({'pd.UInt64 - datetime': pd.Series([20240101, 20240630, 20250101, None, 20250601, 20260101], dtype='UInt64')}),
     'pd.Float32 - datetime': pd.DataFrame({
-        'pd.Float32 - datetime': pd.Series([20240101.0, 20240630.0, 20250101.0, None, 20250601.0, 20260101.0], dtype='Float32')
+        'pd.Float32 - datetime': pd.Series([20240101, 20240630, 20250101, None, 20250601, 20260101], dtype='Float32')
     }),
     'pd.Float64 - datetime': pd.DataFrame({
         'pd.Float64 - datetime': pd.Series([20240101.0, 20240630.0, 20250101.0, None, 20250601.0, 20260101.0], dtype='Float64')
@@ -64,6 +60,6 @@ PANDAS_DATETIME_DTYPES = {
         'pd.datetime64 - datetime': pd.Series(pd.date_range('2023-01-01', periods=3), dtype='datetime64[ns]')
     }),
     'pd.Period - datetime': pd.DataFrame({
-        'pd.Period - datetime': pd.Series(pd.period_range('2023-01', periods=3, freq='M')),
+        'pd.Period - datetime': pd.Series(pd.period_range('2023-01-01', periods=3, freq='D')),
     }),
 }
