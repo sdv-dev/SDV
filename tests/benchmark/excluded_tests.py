6eca0d4546db940511cc785ed4aac829ebe25acc
rwedge <roy.wedge@datacebo.com>
2025-04-11 10:50:17 -0500

    update excluded test; add pandas dt dtypes

--- tests/benchmark/excluded_tests.py
+++ tests/benchmark/excluded_tests.py
@@ -1,4 +1,6 @@
 """Excluded test combinations."""
+from tests.benchmark.numpy_dtypes import NUMPY_DTYPES, NUMPY_DATETIME_DTYPES
+from tests.benchmark.pandas_dtypes import PANDAS_DTYPES, PANDAS_DATETIME_DTYPES
 
 EXCLUDED_DATA_TYPES = {
     ('pd.boolean', 'numerical'),
@@ -21,3 +23,12 @@ EXCLUDED_DATA_TYPES = {
     ('np.unicode', 'datetime'),
     ('np.timedelta64', 'datetime'),
 }
+
+for dtypes in (NUMPY_DTYPES, PANDAS_DTYPES):
+    for dtype in dtypes:
+        EXCLUDED_DATA_TYPES.add((dtype, 'datetime'))
+
+for dtypes in (NUMPY_DATETIME_DTYPES, PANDAS_DATETIME_DTYPES):
+    for dtype in dtypes:
+        for sdtype in ('numerical', 'id', 'categorical'):
+            EXCLUDED_DATA_TYPES.add((dtype, sdtype))
