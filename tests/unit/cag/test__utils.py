a20a37d0822b04387253d7943b57f8481b72168d
gsheni <gvsheni@gmail.com>
2025-05-05 10:37:17 -0400

    remove unused func

--- tests/unit/cag/test__utils.py
+++ tests/unit/cag/test__utils.py
@@ -9,11 +9,9 @@ from sdv.cag._errors import PatternNotMetError
 from sdv.cag._utils import (
     _convert_to_snake_case,
     _is_list_of_type,
-    _remove_columns_from_metadata,
     _validate_table_and_column_names,
     _validate_table_name_if_defined,
 )
-from sdv.metadata.metadata import Metadata
 
 
 def test__validate_table_and_column_names():
@@ -74,123 +72,6 @@ def test__validate_table_name_if_defined_raises():
     expected_table_name_str_or_none = '`table_name` must be a string or None.'
     with pytest.raises(ValueError, match=expected_table_name_str_or_none):
         _validate_table_name_if_defined(table_name=1)
-
-
-def test__remove_columns_from_metadata_single():
-    """Test `_remove_columns_from_metadata` method removes columns from metadata (single-table)"""
-    # Setup
-    original_metadata = Metadata.load_from_dict({
-        'tables': {
-            'table': {
-                'columns': {
-                    'country_column': {'sdtype': 'categorical'},
-                    'city_column': {'sdtype': 'categorical'},
-                },
-                'column_relationships': [
-                    {'type': 'address', 'column_names': ['country_column', 'city_column']}
-                ],
-            }
-        },
-        'relationships': [],
-        'METADATA_SPEC_VERSION': 'V1',
-    })
-
-    # Run
-    column_to_drop = 'country_column'
-    new_metadata = _remove_columns_from_metadata(
-        metadata=original_metadata,
-        table_name='table',
-        columns_to_drop=[column_to_drop],
-    )
-
-    # Assert
-    assert isinstance(new_metadata, Metadata)
-    assert column_to_drop in original_metadata.tables['table'].columns
-    assert (
-        column_to_drop in original_metadata.tables['table'].column_relationships[0]['column_names']
-    )
-    assert column_to_drop not in new_metadata.tables['table'].columns
-    assert 'city_column' in new_metadata.tables['table'].columns
-    assert len(new_metadata.tables['table'].column_relationships) == 0
-
-
-def test__remove_columns_from_metadata_multi():
-    """Test `_remove_columns_from_metadata` method removes columns from metadata (multi-table)"""
-    # Setup
-    original_metadata = Metadata.load_from_dict({
-        'tables': {
-            'parent': {
-                'primary_key': 'id',
-                'columns': {
-                    'id': {'sdtype': 'id'},
-                    'A': {'sdtype': 'numerical'},
-                    'B': {'sdtype': 'numerical'},
-                },
-                'column_relationships': [{'type': 'gps', 'column_names': ['A', 'B']}],
-            },
-            'child': {
-                'primary_key': 'id',
-                'columns': {
-                    'id': {'sdtype': 'id'},
-                },
-            },
-        },
-        'relationships': [
-            {
-                'parent_table_name': 'parent',
-                'parent_primary_key': 'id',
-                'child_table_name': 'child',
-                'child_foreign_key': 'id',
-            },
-        ],
-    })
-    columns_to_drop = ['A', 'B']
-
-    # Run
-    new_metadata = _remove_columns_from_metadata(
-        metadata=original_metadata,
-        table_name='parent',
-        columns_to_drop=columns_to_drop,
-    )
-
-    # Assert
-    assert isinstance(new_metadata, Metadata)
-    for column in columns_to_drop:
-        assert column in original_metadata.tables['parent'].columns
-        assert column in original_metadata.tables['parent'].column_relationships[0]['column_names']
-
-        assert column not in new_metadata.tables['parent'].columns
-        assert len(new_metadata.tables['parent'].column_relationships) == 0
-
-
-def test__remove_columns_from_metadata_raises_pk():
-    """Test `_remove_columns_from_metadata` method raises an error if primary key is dropped"""
-    # Setup
-    original_metadata = Metadata.load_from_dict({
-        'tables': {
-            'parent': {
-                'primary_key': 'id',
-                'columns': {'id': {'sdtype': 'id'}},
-            },
-        },
-        'relationships': [
-            {
-                'parent_table_name': 'parent',
-                'parent_primary_key': 'id',
-                'child_table_name': 'child',
-                'child_foreign_key': 'id',
-            },
-        ],
-    })
-
-    # Run and Assert
-    cannot_remove_pk = 'Cannot remove primary key from Metadata'
-    with pytest.raises(ValueError, match=cannot_remove_pk):
-        _remove_columns_from_metadata(
-            metadata=original_metadata,
-            table_name='parent',
-            columns_to_drop=['id'],
-        )
 
 
 def test__is_list_of_type():
