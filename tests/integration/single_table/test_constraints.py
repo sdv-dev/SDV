391d7243ed0ea3daf87f150cad2a3644fd1b642c
R-Palazzo <romainpalazzo@gmail.com>
2025-05-08 13:35:49 +0100

    add test backward compatibility save/load

--- tests/integration/single_table/test_constraints.py
+++ tests/integration/single_table/test_constraints.py
@@ -1023,3 +1023,39 @@ def test_constraint_datetime_check():
         'high_col': ['05 Sep, 15', '26 Sep, 15', '12 Aug, 15'],
     })
     pd.testing.assert_frame_equal(samples, expected_dataframe)
+
+
+def test_backward_compatibility_old_style_constraints(tmpdir):
+    """Test that the old-style constraints are still supported."""
+    # Setup
+    data = pd.DataFrame({
+        'A': [1, 2, 3],
+        'B': [4, 5, 6],
+    })
+
+    metadata = Metadata()
+    metadata.add_table('table')
+    metadata.add_column('A', 'table', sdtype='numerical')
+    metadata.add_column('B', 'table', sdtype='numerical')
+
+    synthesizer = GaussianCopulaSynthesizer(metadata)
+    constraint = {
+        'constraint_class': 'Inequality',
+        'constraint_parameters': {
+            'low_column_name': 'A',
+            'high_column_name': 'B',
+            'strict_boundaries': False,
+        },
+    }
+    synthesizer._data_processor.add_constraints([constraint])
+
+    # Run
+    synthesizer.fit(data)
+    samples = synthesizer.sample(len(data))
+    synthesizer.save(tmpdir / 'test.pkl')
+    synthesizer_loaded = GaussianCopulaSynthesizer.load(tmpdir / 'test.pkl')
+    samples_loaded = synthesizer_loaded.sample(len(data))
+
+    # Assert
+    assert all(samples['A'] < samples['B'])
+    assert all(samples_loaded['A'] < samples_loaded['B'])
