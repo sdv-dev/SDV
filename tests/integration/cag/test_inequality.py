6288b9446a4c3064347b55ce9a1b692080e95e67
R-Palazzo <romainpalazzo@gmail.com>
2025-05-09 17:56:54 +0100

    fix tests

--- tests/integration/cag/test_inequality.py
+++ tests/integration/cag/test_inequality.py
@@ -684,9 +684,7 @@ def test_inequality_multiple_patterns_reject_sampling(
         }
     }).to_dict()
     assert expected_updated_metadata == updated_metadata.to_dict()
-
     assert original_metadata.to_dict() == metadata.to_dict()
-
     assert all(samples['low'] <= samples['high'])
 
 
