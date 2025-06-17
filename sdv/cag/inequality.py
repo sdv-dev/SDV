cb8e92caf15cae1eb7dfc029ad26460208ac4f9e
Frances Hartwell <frances@datacebo.com>
2025-06-17 18:30:49 -0400

    Fix Inequality using self.metadata

--- sdv/cag/inequality.py
+++ sdv/cag/inequality.py
@@ -306,7 +306,7 @@ class Inequality(BaseConstraint):
             dict[str, pd.Series]:
                 Whether each row is valid.
         """
-        table_name = self._get_single_table_name(self.metadata)
+        table_name = self._get_single_table_name(metadata)
         is_valid = _get_is_valid_dict(data, table_name)
         valid_table_rows = self._get_valid_table_data(data[table_name], metadata, table_name)
         is_valid[table_name] = valid_table_rows
