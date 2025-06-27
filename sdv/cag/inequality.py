d2b980f7c8b80e9ac4d4ae661aa492508162a8ff
R-Palazzo <romainpalazzo@gmail.com>
2025-06-27 17:44:08 +0100

    fix after rebase

--- sdv/cag/inequality.py
+++ sdv/cag/inequality.py
@@ -146,7 +146,9 @@ class Inequality(BaseConstraint):
         table_name = self._get_single_table_name(metadata)
         valid = self._get_valid_table_data(data[table_name], metadata, table_name)
         if not valid.all():
-            invalid_rows = data.loc[~valid, [self._low_column_name, self._high_column_name]]
+            invalid_rows = data[table_name].loc[
+                ~valid, [self._low_column_name, self._high_column_name]
+            ]
             error_message = self._format_error_message_constraint(invalid_rows, table_name)
             raise ConstraintNotMetError(error_message)
 
