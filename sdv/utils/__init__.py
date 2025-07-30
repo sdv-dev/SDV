647b5c9b0f714d191f43e717d95526a45162f9ce
R-Palazzo <116157184+R-Palazzo@users.noreply.github.com>
2025-07-30 12:49:14 +0100

    Streamline the loading of a synthesizer into a single function (#2624)

--- sdv/utils/__init__.py
+++ sdv/utils/__init__.py
@@ -1,5 +1,5 @@
 """Utils module."""
 
-from sdv.utils.utils import drop_unknown_references, get_random_sequence_subset
+from sdv.utils.utils import drop_unknown_references, get_random_sequence_subset, load_synthesizer
 
-__all__ = ('drop_unknown_references', 'get_random_sequence_subset')
+__all__ = ('drop_unknown_references', 'get_random_sequence_subset', 'load_synthesizer')
