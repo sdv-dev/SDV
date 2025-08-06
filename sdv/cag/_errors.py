654a5d15cf3094d9fee9d4e51e74ec30729d02b7
R-Palazzo <romainpalazzo@gmail.com>
2025-08-06 11:02:12 +0100

    define ConstraintNotMetWarning

--- sdv/cag/_errors.py
+++ sdv/cag/_errors.py
@@ -3,3 +3,6 @@
 
 class ConstraintNotMetError(Exception):
     """Error to raise when a constraint is not met."""
+
+class ConstraintNotMetWarning(Warning):
+    """Warning to raise when a constraint is not met but the user can continue."""
