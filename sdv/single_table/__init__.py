7e0e9f9611cbb0f2a75834658765382c6eb9ccaf
R-Palazzo <romainpalazzo@gmail.com>
2025-09-05 23:01:08 +0200

    def 2666

--- sdv/single_table/__init__.py
+++ sdv/single_table/__init__.py
@@ -3,10 +3,12 @@
 from sdv.single_table.copulagan import CopulaGANSynthesizer
 from sdv.single_table.copulas import GaussianCopulaSynthesizer
 from sdv.single_table.ctgan import CTGANSynthesizer, TVAESynthesizer
+from sdv.single_table.dayz import DayZSynthesizer
 
 __all__ = (
     'GaussianCopulaSynthesizer',
     'CTGANSynthesizer',
     'TVAESynthesizer',
     'CopulaGANSynthesizer',
+    'DayZSynthesizer',
 )
