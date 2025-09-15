204c1ecaebf0ac01e37cfb623fbcf99c0d86a4b3
R-Palazzo <romainpalazzo@gmail.com>
2025-09-15 16:25:03 -0400

    define _dayz_utils file

--- tests/unit/multi_table/test_dayz.py
+++ tests/unit/multi_table/test_dayz.py
@@ -1,4 +1,3 @@
-import json
 import re
 from unittest.mock import patch
 
@@ -7,110 +6,7 @@ import pytest
 
 from sdv.errors import SynthesizerInputError
 from sdv.metadata import Metadata
-from sdv.multi_table.dayz import (
-    DayZSynthesizer,
-    create_parameters_multi_table,
-    detect_relationship_parameters,
-)
-
-
-def test_detect_relationship_parameters():
-    """Test the `detect_relationship_parameters` method."""
-    # Setup
-    parent_data = pd.DataFrame({'parent_id': [1, 2, 3, 4, 5]})
-    child_data = pd.DataFrame({
-        'child_id': [10, 11, 12, 13, 14, 15, 16],
-        'parent_id': [1, 1, 2, 2, 2, 3, None],
-    })
-    data = {'parent': parent_data, 'child': child_data}
-    metadata_dict = {
-        'tables': {
-            'parent': {'columns': {'parent_id': {'sdtype': 'id'}}, 'primary_key': 'parent_id'},
-            'child': {
-                'columns': {'child_id': {'sdtype': 'id'}, 'parent_id': {'sdtype': 'id'}},
-                'primary_key': 'child_id',
-            },
-        },
-        'relationships': [
-            {
-                'parent_table_name': 'parent',
-                'child_table_name': 'child',
-                'parent_primary_key': 'parent_id',
-                'child_foreign_key': 'parent_id',
-            }
-        ],
-    }
-    metadata = Metadata.load_from_dict(metadata_dict)
-
-    # Run
-    result = detect_relationship_parameters(data, metadata)
-
-    # Assert
-    expected = {
-        '["parent", "child", "parent_id", "parent_id"]': {
-            'min_cardinality': 0,
-            'max_cardinality': 3,
-        }
-    }
-    assert result == expected
-
-
-@patch('sdv.multi_table.dayz.detect_relationship_parameters')
-@patch('sdv.multi_table.dayz.create_parameters')
-def test_create_parameters_multi_table(mock_create_parameters, mock_detect_relationship, tmp_path):
-    """Test the `create_parameters_multi_table` method."""
-    # Setup
-    data = pd.DataFrame()
-    metadata = Metadata()
-    output_filename = str(tmp_path / 'output.json')
-    mock_detect_relationship.return_value = {
-        '["parent_table", "child_table", "parent_pk", "child_fk"]': {
-            'min_cardinality': 0,
-            'max_cardinality': 10,
-        }
-    }
-    mock_create_parameters.return_value = {
-        'DAYZ_SPEC_VERSION': 'V1',
-        'tables': {
-            'table_name': {
-                'num_rows': 100,
-                'columns': {
-                    'col1': {'missing_value_proportion': 0.1},
-                    'col2': {'missing_value_proportion': 0.2},
-                },
-            }
-        },
-    }
-
-    # Run
-    result = create_parameters_multi_table(data, metadata, output_filename)
-
-    # Assert
-    mock_create_parameters.assert_called_once_with(data, metadata, None)
-    mock_detect_relationship.assert_called_once_with(data, metadata)
-    assert result == {
-        'DAYZ_SPEC_VERSION': 'V1',
-        'tables': {
-            'table_name': {
-                'num_rows': 100,
-                'columns': {
-                    'col1': {'missing_value_proportion': 0.1},
-                    'col2': {'missing_value_proportion': 0.2},
-                },
-            }
-        },
-        'relationships': {
-            '["parent_table", "child_table", "parent_pk", "child_fk"]': {
-                'min_cardinality': 0,
-                'max_cardinality': 10,
-            }
-        },
-    }
-    assert result == mock_create_parameters.return_value
-    with open(output_filename) as f:
-        output = json.load(f)
-
-    assert output == result
+from sdv.multi_table.dayz import DayZSynthesizer
 
 
 class TestDayZSynthesizer:
--- tests/unit/single_table/test_dayz.py
+++ tests/unit/single_table/test_dayz.py
@@ -1,125 +1,12 @@
-import json
 import re
-from unittest.mock import Mock, patch
+from unittest.mock import patch
 
 import pandas as pd
 import pytest
 
 from sdv.errors import SynthesizerInputError
 from sdv.metadata import Metadata
-from sdv.single_table.dayz import (
-    DayZSynthesizer,
-    create_parameters,
-    detect_column_parameters,
-    detect_table_parameters,
-)
-
-
-def test_detect_table_parameters():
-    """Test the `detect_table_parameters` method."""
-    # Setup
-    data = pd.DataFrame(index=range(10))
-
-    # Run
-    result = detect_table_parameters(data)
-
-    # Assert
-    assert result == {'num_rows': 10}
-
-
-def test_detect_column_parameter():
-    """Test the `detect_column_parameters` method."""
-    # Setup
-    data = pd.DataFrame({
-        'num_col': [1.0, 2.5, 3.0, None],
-        'cat_col': ['A', 'B', 'A', None],
-        'date_col': ['2020-01-01', '2020-01-02', None, None],
-        'date_col_2': ['2020 Jan 01', '2020 Jan 02', '2020 Jan 03', None],
-    })
-    metadata = Metadata.load_from_dict({
-        'tables': {
-            'table_name': {
-                'columns': {
-                    'num_col': {'sdtype': 'numerical'},
-                    'cat_col': {'sdtype': 'categorical'},
-                    'date_col': {'sdtype': 'datetime', 'datetime_format': '%Y-%m-%d'},
-                    'date_col_2': {'sdtype': 'datetime'},
-                }
-            }
-        }
-    })
-    # Run
-    result = detect_column_parameters(data, metadata, 'table_name')
-
-    # Assert
-    assert result == {
-        'columns': {
-            'num_col': {
-                'num_decimal_digits': 1,
-                'min_value': 1.0,
-                'max_value': 3.0,
-                'missing_value_proportion': 0.25,
-            },
-            'cat_col': {
-                'category_values': ['A', 'B'],
-                'missing_value_proportion': 0.25,
-            },
-            'date_col': {
-                'start_timestamp': '2020-01-01',
-                'end_timestamp': '2020-01-02',
-                'missing_value_proportion': 0.5,
-            },
-            'date_col_2': {
-                'start_timestamp': '2020-01-01 00:00:00',
-                'end_timestamp': '2020-01-03 00:00:00',
-                'missing_value_proportion': 0.25,
-            },
-        }
-    }
-
-
-@patch('sdv.single_table.dayz.detect_column_parameters')
-@patch('sdv.single_table.dayz.detect_table_parameters')
-def test_create_parameters(mock_detect_table, mock_detect_column, tmp_path):
-    """Test the `create_parameters` method."""
-    # Setup
-    output_filename = tmp_path / 'output.json'
-    mock_detect_table.return_value = {'num_rows': 100}
-    mock_detect_column.return_value = {
-        'columns': {
-            'col1': {'missing_value_proportion': 0.1},
-            'col2': {'missing_value_proportion': 0.2},
-        }
-    }
-
-    data = pd.DataFrame()
-    metadata = Mock()
-    metadata._get_single_table_name.return_value = 'table_name'
-
-    # Run
-    result = create_parameters(data, metadata, output_filename=output_filename)
-
-    # Assert
-    metadata.validate.assert_called_once()
-    metadata.validate_data.assert_called_once_with({'table_name': data})
-    mock_detect_table.assert_called_once_with(data)
-    mock_detect_column.assert_called_once_with(data, metadata, 'table_name')
-    assert result == {
-        'DAYZ_SPEC_VERSION': 'V1',
-        'tables': {
-            'table_name': {
-                'num_rows': 100,
-                'columns': {
-                    'col1': {'missing_value_proportion': 0.1},
-                    'col2': {'missing_value_proportion': 0.2},
-                },
-            }
-        },
-    }
-    with open(output_filename, 'r') as f:
-        output = json.load(f)
-
-    assert output == result
+from sdv.single_table.dayz import DayZSynthesizer
 
 
 class TestDayZSynthesizer:
