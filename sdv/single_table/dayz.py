82220392a981483c045054bf14d71b5cfe528c8a
Frances Hartwell <frances@datacebo.com>
2025-10-16 08:49:01 -0400

    [DayZ Parameters] 'missing_values_proportion' should not be valid for any key columns (#2709)
    Co-authored-by: R-Palazzo <116157184+R-Palazzo@users.noreply.github.com>

--- sdv/single_table/dayz.py
+++ sdv/single_table/dayz.py
@@ -132,7 +132,7 @@ def _validate_categorical_parameters(column_parameters, column_table_msg):
         raise SynthesizerProcessingError(msg)
 
 
-def _validate_missing_value_parameters(column_parameters, column_table_msg):
+def _validate_missing_value_parameters(column_parameters, column_table_msg, is_key_column):
     missing_values_proportion = column_parameters['missing_values_proportion']
     if not _is_numerical(missing_values_proportion) or (
         missing_values_proportion < 0.0 or missing_values_proportion > 1.0
@@ -142,9 +142,15 @@ def _validate_missing_value_parameters(column_parameters, column_table_msg):
             'must be a float between 0.0 and 1.0.'
         )
         raise SynthesizerProcessingError(msg)
+    elif is_key_column and missing_values_proportion != 0:
+        msg = (
+            f"Invalid 'missing_values_proportion' parameter for {column_table_msg}. Primary "
+            "and alternate keys must have 'missing_values_proportion' parameter set to zero."
+        )
+        raise SynthesizerProcessingError(msg)
 
 
-def _validate_column_parameters(table, column, column_metadata, column_parameters):
+def _validate_column_parameters(table, column, column_metadata, column_parameters, is_key_column):
     column_table_msg = f"column '{column}' in table '{table}'"
     sdtype = column_metadata['sdtype']
     sdtype_parameters = SDTYPE_TO_PARAMETERS.get(sdtype, COLUMN_PARAMETER_KEYS)
@@ -165,7 +171,7 @@ def _validate_column_parameters(table, column, column_metadata, column_parameter
         _validate_categorical_parameters(column_parameters, column_table_msg)
 
     if 'missing_values_proportion' in column_parameters:
-        _validate_missing_value_parameters(column_parameters, column_table_msg)
+        _validate_missing_value_parameters(column_parameters, column_table_msg, is_key_column)
 
 
 def _validate_table_parameters(table, table_metadata, table_parameters):
@@ -186,9 +192,11 @@ def _validate_table_parameters(table, table_metadata, table_parameters):
         )
         raise SynthesizerProcessingError(msg)
 
+    key_columns = table_metadata._get_primary_and_alternate_keys()
     for column, column_parameters in table_parameters.get('columns', {}).items():
+        is_key_column = column in key_columns
         _validate_column_parameters(
-            table, column, table_metadata.columns[column], column_parameters
+            table, column, table_metadata.columns[column], column_parameters, is_key_column
         )
 
 
--- tests/unit/single_table/test__dayz_utils.py
+++ tests/unit/single_table/test__dayz_utils.py
@@ -27,20 +27,26 @@ def test_detect_column_parameter():
     """Test the `detect_column_parameters` method."""
     # Setup
     data = pd.DataFrame({
+        'pk': [0, 1, 2, 3],
         'num_col': [1.0, 2.5, 3.0, None],
         'cat_col': ['A', 'B', 'A', None],
         'date_col': ['2020-01-01', '2020-01-02', None, None],
         'date_col_2': ['2020 Jan 01', '2020 Jan 02', '2020 Jan 03', None],
+        'alt_key': ['id0', 'id1', 'id2', 'id3'],
     })
     metadata = Metadata.load_from_dict({
         'tables': {
             'table_name': {
                 'columns': {
+                    'pk': {'sdtype': 'id'},
                     'num_col': {'sdtype': 'numerical'},
                     'cat_col': {'sdtype': 'categorical'},
                     'date_col': {'sdtype': 'datetime', 'datetime_format': '%Y-%m-%d'},
                     'date_col_2': {'sdtype': 'datetime'},
-                }
+                    'alt_key': {'sdtype': 'ssn'},
+                },
+                'primary_key': 'pk',
+                'alternate_keys': ['alt_key'],
             }
         }
     })
@@ -50,6 +56,7 @@ def test_detect_column_parameter():
     # Assert
     assert result == {
         'columns': {
+            'pk': {'missing_values_proportion': 0.0},
             'num_col': {
                 'num_decimal_digits': 1,
                 'min_value': 1.0,
@@ -70,6 +77,7 @@ def test_detect_column_parameter():
                 'end_timestamp': '2020-01-03 00:00:00',
                 'missing_values_proportion': 0.25,
             },
+            'alt_key': {'missing_values_proportion': 0.0},
         }
     }
 
