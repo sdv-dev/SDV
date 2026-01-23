684eacefcc37c68fa93e638cac461bfc2254cdf7
Gaurav Sheni <gvsheni@gmail.com>
2026-01-23 17:45:05 -0500

    cleanup

--- sdv/metadata/multi_table.py
+++ sdv/metadata/multi_table.py
@@ -160,17 +160,6 @@ class MultiTableMetadata:
                 f'tables {errors}.'
             )
 
-    def _validate_foreign_child_key(self, child_table_name, parent_table_name, child_foreign_key):
-        pass
-        # child_primary_key = _cast_to_iterable(self.tables[child_table_name].primary_key)
-        # child_foreign_key = _cast_to_iterable(child_foreign_key)
-        # if set(child_foreign_key).intersection(set(child_primary_key)):
-        #     raise InvalidMetadataError(
-        #         f"Invalid relationship between table '{parent_table_name}' and table "
-        #         f"'{child_table_name}'. A relationship must connect a primary key "
-        #         'with a non-primary key.'
-        #     )
-
     def _validate_new_foreign_key_is_not_reused(
         self, parent_table_name, parent_primary_key, child_table_name, child_foreign_key
     ):
@@ -238,8 +227,6 @@ class MultiTableMetadata:
         self._validate_relationship_key_length(
             parent_table_name, parent_primary_key, child_table_name, child_foreign_key
         )
-
-        self._validate_foreign_child_key(child_table_name, parent_table_name, child_foreign_key)
 
         self._validate_relationship_sdtypes(
             parent_table_name, parent_primary_key, child_table_name, child_foreign_key
