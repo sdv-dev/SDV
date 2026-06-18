b030434ee5561b62c019c5f699d11652d65a4f74
Frances Hartwell <frances@datacebo.com>
2026-06-18 13:51:45 -0400

    Add backwards compatability for SingleTableProgrammableConstraint

--- tests/integration/cag/test_programmable_constraint.py
+++ tests/integration/cag/test_programmable_constraint.py
@@ -3,14 +3,14 @@
 import pandas as pd
 import pytest
 
-from sdv.cag import FixedCombinations, ProgrammableConstraint
+from sdv.cag import FixedCombinations, ProgrammableConstraint, SingleTableProgrammableConstraint
 from sdv.datasets.demo import download_demo
 from sdv.metadata import Metadata
 from sdv.multi_table import HMASynthesizer
 from sdv.single_table import GaussianCopulaSynthesizer
 
 
-class SingleTableIfTrueThenZero(ProgrammableConstraint):
+class IfTrueThenZero(ProgrammableConstraint):
     """Custom constraint that ensures that if a flag column is True."""
 
     def __init__(self, target_column, flag_column, table_name=None):
@@ -100,6 +100,55 @@ def programmable_constraint():
     return MyConstraint
 
 
+@pytest.fixture
+def deprecated_single_table_programmable_constraint():
+    """Old-style SingleTableProgrammableConstraint for backwards compatability tests."""
+
+    class MyConstraint(SingleTableProgrammableConstraint):
+        def __init__(self, column_names, table_name):
+            self.column_names = column_names
+            self.table_name = table_name
+            self._joint_column = '#'.join(self.column_names)
+            self._combinations = None
+            self._fitted = False
+
+        def _get_single_table_name(self, metadata):
+            # Have to define this so that we can re-use existing methods on the constraint
+            return self.table_name
+
+        def validate(self, metadata):
+            FixedCombinations._validate_constraint_with_metadata(self, metadata)
+
+        def validate_input_data(self, data):
+            return
+
+        def fit(self, data, metadata):
+            self.metadata = metadata
+            data = {self.table_name: data}
+            FixedCombinations._fit(self, data, metadata)
+            self._fitted = True
+
+        def transform(self, data):
+            data = {self.table_name: data}
+            transformed = FixedCombinations._transform(self, data)
+            return transformed[self.table_name]
+
+        def get_updated_metadata(self, metadata):
+            return FixedCombinations._get_updated_metadata(self, metadata)
+
+        def reverse_transform(self, transformed_data):
+            transformed_data = {self.table_name: transformed_data}
+            reverse_transformed = FixedCombinations._reverse_transform(self, transformed_data)
+            return reverse_transformed[self.table_name]
+
+        def is_valid(self, synthetic_data):
+            synthetic_data = {self.table_name: synthetic_data}
+            is_valid = FixedCombinations._is_valid(self, synthetic_data, self.metadata)
+            return is_valid[self.table_name]
+
+    return MyConstraint
+
+
 def test_end_to_end_programmable_constraint_multi_table(programmable_constraint):
     """Test using a programmable constraint with a multi table synthesizer end-to-end."""
     data, metadata = download_demo('multi_table', 'fake_hotels')
@@ -143,12 +192,34 @@ def test_end_to_end_programmable_constraint_single_table(programmable_constraint
     assert isinstance(constraints[0], programmable_constraint)
 
 
+def test_end_to_end_deprecated_programmable_single_table_constraint(
+    deprecated_single_table_programmable_constraint,
+):
+    """Test using a deprecated single table programmable constraint end-to-end."""
+    data, metadata = download_demo('single_table', 'fake_hotel_guests')
+    my_constraint = deprecated_single_table_programmable_constraint(
+        column_names=['has_rewards', 'room_type'], table_name='fake_hotel_guests'
+    )
+    synthesizer = GaussianCopulaSynthesizer(metadata)
+    synthesizer.add_constraints([my_constraint])
+
+    # Run
+    synthesizer.fit(data)
+    sampled_data = synthesizer.sample(1000)
+    constraints = synthesizer.get_constraints()
+
+    # Assert
+    original_combinations = set(zip(data['has_rewards'], data['room_type']))
+    assert set(zip(sampled_data['has_rewards'], sampled_data['room_type'])) == original_combinations
+    assert isinstance(constraints[0], deprecated_single_table_programmable_constraint)
+
+
 def test_end_to_end_simple_constraint_with_no_fit(programmable_constraint):
     """Test using a programmable constraint without a fit method."""
     # Setup
     data, metadata = download_demo('single_table', 'fake_hotel_guests')
     synthesizer = GaussianCopulaSynthesizer(metadata)
-    custom_constraint = SingleTableIfTrueThenZero(
+    custom_constraint = IfTrueThenZero(
         target_column='amenities_fee',
         flag_column='has_rewards',
         table_name=metadata._get_single_table_name(),
@@ -206,3 +277,12 @@ def test_get_updated_metadata_is_passed_metadata_copy(programmable_constraint):
     })
     expected_modified_metadata.add_column('has_rewards#room_type', sdtype='categorical')
     assert updated_metadata.to_dict() == expected_modified_metadata.to_dict()
+
+
+def test_single_table_programmable_constraint_backwards_compatible():
+    """Test that old-style SingleTableProgrammableConstraints still work."""
+    # Setup
+
+    # Run
+
+    # Assert
