bb3466d19342d569855a1f1a0e30328c55b0e3fe
Andrew Montanez <andrew@sdv.dev>
2026-08-14 12:08:44 -0500

    Remove POC Module (#2945)

--- sdv/utils/__init__.py
+++ sdv/utils/__init__.py
@@ -3,13 +3,17 @@
 from sdv.utils.utils import (
     drop_unknown_references,
     get_random_sequence_subset,
-    load_synthesizer,
+    get_random_subset,
     load_constraints,
+    load_synthesizer,
+    simplify_schema,
 )
 
 __all__ = (
     'drop_unknown_references',
     'get_random_sequence_subset',
+    'get_random_subset',
     'load_synthesizer',
     'load_constraints',
+    'simplify_schema',
 )
