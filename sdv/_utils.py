fee20ec816fef5fecfda28d4bcae68145fe45df8
R-Palazzo <romainpalazzo@gmail.com>
2026-09-03 15:46:13 +0100

    cleaning

--- sdv/_utils.py
+++ sdv/_utils.py
@@ -685,6 +685,7 @@ def _validate_data_single_table(data, table_name=None):
             'The `data` parameter must be a dictionary containing exactly one table name '
             'mapped to a pandas DataFrame.'
         )
+
     if table_name is not None and table_name not in data:
         raise InvalidDataTypeError(
             f"The specified table name '{table_name}' is not present in the data."
